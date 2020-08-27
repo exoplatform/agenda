2d86ed3936fe5a5ae9a11239b093acc9968b9b50
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2020-08-27 16:07:14 +0200

    Implement Agenda application Frontend skeleton

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda/components/CreateEventButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn
+    class="btn btn-primary"
+    @click="openEventDialog"
+  >
+    <v-icon dark left>
+      mdi-plus
+    </v-icon>
+    {{ $t('agenda.button.addEvent') }}
+  </v-btn>
+</template>
+<script>
+export default {
+  methods:{
+    openEventDialog(){
+      this.$root.$emit('agenda-open-event-form');
+    },
+  }
+};
+</script>
