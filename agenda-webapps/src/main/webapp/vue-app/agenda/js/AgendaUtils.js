e3bde5e8fbc3c5c5e809273ff46187979b142c01
aelbenney <aelbenney@exoplatform.com>
2020-08-28 14:14:07 +0100

    implement agenda week,month and day view

--- agenda-webapps/src/main/webapp/vue-app/agenda/js/AgendaUtils.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda/js/AgendaUtils.js
@@ -1,13 +1,30 @@
 export function getUserTimezone() {
-  const timezoneHours = eXo.env.portal.timezoneOffset / 3600000;
-  const timezoneMinutes = eXo.env.portal.timezoneOffset % 3600000 / 60000;
+  let timezoneHours = eXo.env.portal.timezoneOffset / 3600000;
+  let timezoneMinutes = eXo.env.portal.timezoneOffset % 3600000 / 60000;
+  timezoneHours = timezoneHours < 10 ? `0${timezoneHours}` : timezoneHours;
+  timezoneMinutes = timezoneMinutes < 10 ? `0${timezoneMinutes}` : timezoneMinutes;
   const timezoneSign = eXo.env.portal.timezoneOffset >= 0 ? '+' : '-';
   return `${timezoneSign}${timezoneHours}:${timezoneMinutes}`;
 }
 
 export function convertVuetifyRangeToPeriod(range, userTimezone) {
+  const rangeStartHour = range.start.hour < 10 ? `0${range.start.hour}` : range.start.hour;
+  const rangeStartMinute = range.start.minute < 10 ? `0${range.start.minute}` : range.start.minute;
+  const rangeEndHour = range.end.hour < 10 ? `0${range.end.hour}` : range.start.hour;
+  const rangeEndMinute = range.end.minute < 10 ? `0${range.end.minute}` : range.start.minute;
   return {
-    start: `${range.start.year}-${range.start.month}-${range.start.day}T${range.start.hour}:${range.start.minute}:${range.start.second}.000${userTimezone}`,
-    end: `${range.end.year}-${range.end.month}-${range.end.day}T${range.end.hour}:${range.end.minute}:${range.end.second}.000${userTimezone}`,
+    start: `${range.start.date}T${rangeStartHour}:${rangeStartMinute}:00.000${userTimezone}`,
+    end: `${range.end.date}T${rangeEndHour}:${rangeEndMinute}:00.000${userTimezone}`,
   };
+}
+
+export function getWeekNumber(date) {
+  const dayNumber = (date.getDay() + 6) % 7;
+  date.setDate(date.getDate() - dayNumber + 3);
+  const firstThursday = date.valueOf();
+  date.setMonth(0, 1);
+  if (date.getDay() !== 4) {
+    date.setMonth(0, 1 + (4 - date.getDay() + 7) % 7);
+  }
+  return Math.ceil((firstThursday - date.valueOf()) / 604800000);
 }
