c343954aff5d61cf80bb8b61e2fe6df8699a9eee
El kamel Hassen <helkamel@exoplatform.com>
2020-09-04 11:09:20 +0200

    use flex direction instead of vuetify helpers

--- agenda-webapps/src/main/webapp/vue-app/agenda/components/event/AgendaEventFormBasicInformation.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda/components/event/AgendaEventFormBasicInformation.vue
@@ -1,85 +1,77 @@
 <template>
   <v-container ref="agendaEventForm" class="event-form">
-    <div class="row">
-      <div>
-        <label class="float-left mt-5 mr-3 text-subtitle-1">Create</label>
-        <input
-          id="eventTitle"
-          ref="eventTitle"
-          v-model="event.summary"
-          :placeholder="$t('agenda.eventTitle')"
-          type="text"
-          name="title"
-          class="ignore-vuetify-classes my-3"
-          required>
-      </div>
-      <div>
-        <span class="mt-5  ml-4 mr-4 text-subtitle-1 font-weight-bold">in</span>
-        <exo-identity-suggester
-          id="calendarOwnerAutocomplete"
-          ref="calendarOwnerId"
-          v-model="event.calendar.owner.id"
-          :labels="calendarSuggesterLabels"
-          :include-users="false"
-          name="calendarOwnerAutocomplete"
-          class="user-suggester"
-          include-spaces
-          multiple />
-      </div>
+    <div class="d-flex flex-row">
+      <label class="float-left mt-5 mr-3 text-subtitle-1">Create</label>
+      <input
+        id="eventTitle"
+        ref="eventTitle"
+        v-model="event.summary"
+        :placeholder="$t('agenda.eventTitle')"
+        type="text"
+        name="title"
+        class="ignore-vuetify-classes my-3"
+        required>
+      <span class="mt-5  ml-4 mr-4 text-subtitle-1 font-weight-bold">in</span>
+      <exo-identity-suggester
+        id="calendarOwnerAutocomplete"
+        ref="calendarOwnerId"
+        v-model="event.calendar.owner.id"
+        :labels="calendarSuggesterLabels"
+        :include-users="false"
+        name="calendarOwnerAutocomplete"
+        class="space-suggester"
+        include-spaces
+        multiple />
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="row">
-          <div>
+    <div class="d-flex flex-row">
+      <div class="d-flex flex-column">
+        <div class="d-flex flex-row">
+          <v-icon size="18" class="mr-11">
+            fas fa-map-marker-alt
+          </v-icon>
+          <input
+            id="eventLocation"
+            ref="eventLocation"
+            v-model="event.location"
+            :placeholder="$t('agenda.eventLocation')"
+            type="text"
+            name="locationEvent"
+            class="ignore-vuetify-classes my-3 location-event-input"
+            required>
+        </div>
+        <div class="d-flex flex-row">
+          <v-list>
             <v-icon size="18" class="mr-11">
-              fas fa-map-marker-alt
+              fas fa-bell
             </v-icon>
-            <input
-              id="eventLocation"
-              ref="eventLocation"
-              v-model="event.location"
-              :placeholder="$t('agenda.eventLocation')"
-              type="text"
-              name="locationEvent"
-              class="ignore-vuetify-classes my-3 location-event-input"
-              required>
-          </div>
+            <v-list-item v-for="notifs in notifications" :key="notifs">
+              <label class="float-left ml-4 mr-4">{{ $t('agenda.label.notifyUsers') }}</label>
+              <input
+                ref="timeNotification"
+                type="text"
+                name="timeNotification"
+                class="ignore-vuetify-classes my-3 time-notification">
+              <select class="width-auto my-auto ml-4 pr-2 subtitle-1 ignore-vuetify-classes d-none d-sm-inline notification-date-option">
+                <option>{{ $t('agenda.option.minutes') }}</option>
+                <option>{{ $t('agenda.option.hours') }}</option>
+                <option>{{ $t('agenda.option.days') }}</option>
+                <option>{{ $t('agenda.option.weeks') }}</option>
+              </select>
+              <span class="ml-4">{{ $t('agenda.label.beforeStart') }}</span>
+              <v-btn
+                color="grey"
+                icon
+                dark
+                @click="removeNotifUser(notifs.id)">
+                <v-icon>
+                  mdi-close
+                </v-icon>
+              </v-btn>
+            </v-list-item>
+            <a class="text-subtitle-1 font-weight-regular add-notification-link" @click="addNotification">{{ $t('agenda.addNotification') }}</a>
+          </v-list>
         </div>
-        <div class="row">
-          <div>
-            <v-list>
-              <v-icon size="18" class="mr-11">
-                fas fa-bell
-              </v-icon>
-              <v-list-item v-for="notifs in notifications" :key="notifs">
-                <label class="float-left ml-4 mr-4">{{ $t('agenda.label.notifyUsers') }}</label>
-                <input
-                  ref="timeNotification"
-                  type="text"
-                  name="timeNotification"
-                  class="ignore-vuetify-classes my-3 time-notification">
-                <select class="width-auto my-auto ml-4 pr-2 subtitle-1 ignore-vuetify-classes d-none d-sm-inline notification-date-option">
-                  <option>{{ $t('agenda.option.minutes') }}</option>
-                  <option>{{ $t('agenda.option.hours') }}</option>
-                  <option>{{ $t('agenda.option.days') }}</option>
-                  <option>{{ $t('agenda.option.weeks') }}</option>
-                </select>
-                <span class="ml-4">{{ $t('agenda.label.beforeStart') }}</span>
-                <v-btn
-                  color="grey"
-                  icon
-                  dark
-                  @click="removeNotifUser(notifs.id)">
-                  <v-icon>
-                    mdi-close
-                  </v-icon>
-                </v-btn>
-              </v-list-item>
-              <a class="text-subtitle-1 font-weight-regular add-notification-link" @click="addNotification">{{ $t('agenda.addNotification') }}</a>
-            </v-list>
-          </div>
-        </div>
-        <div class=" row ">
+        <div class="d-flex flex-row">
           <v-icon size="18" class="mr-11">
             fas fa-redo-alt
           </v-icon>
@@ -90,8 +82,7 @@
             <option>4</option>
           </select>
         </div>
-
-        <div class="row ">
+        <div class="d-flex flex-row">
           <v-icon size="18" class="mr-12">
             fas fa-file-alt
           </v-icon>
@@ -108,8 +99,7 @@
             class="ignore-vuetify-classes my-3 description-event-textarea">
           </textarea>
         </div>
-
-        <div class="row">
+        <div class="d-flex flex-row">
           <v-icon size="18" class="mr-11">
             fas fa-paperclip
           </v-icon>
@@ -128,11 +118,11 @@
           <agenda-file-attachments :attachments-file="files" @files="files = $event" />
         </div>
       </div>
-      <span>
+      <div class="d-flex flex-column ml-5">
         <v-divider vertical />
-      </span>
-      <div class="col ml-5">
-        <div class="row">
+      </div>
+      <div class="d-flex flex-column ml-5">
+        <div class="d-flex flex-row">
           <v-icon size="18" class="mr-2">
             fas fa-users
           </v-icon>
@@ -143,22 +133,22 @@
             :disabled="savingUser"
             name="inviteMembers"
             type-of-relations="user_to_invite"
-            class="ma-4 user-suggester"
+            class="user-suggester"
             include-users
             include-spaces />
         </div>
-        <div class="row">
+        <div class="d-flex flex-row">
           <label class="switch-label-text mt-1 text-subtitle-1 font-weight-bold">{{ $t('agenda.modifyEventPermission') }}</label>
           <v-switch v-model="enablePermission" class="mt-0 ml-4" />
         </div>
-        <div class="row font-weight-regular">
+        <div class="d-flex flex-row font-weight-regular">
           {{ $t('agenda.modifyEventPermissionDescription') }}
         </div>
-        <div class="row">
+        <div class="d-flex flex-row">
           <label class="switch-label-text mt-1 text-subtitle-1 font-weight-bold">{{ $t('agenda.enableInvitation') }}</label>
           <v-switch v-model="enableInvitation" class="mt-0 ml-4" />
         </div>
-        <div class="row font-weight-regular">
+        <div class="d-flex flex-row font-weight-regular">
           {{ $t('agenda.enableInvitationDescription') }}
         </div>
       </div>
