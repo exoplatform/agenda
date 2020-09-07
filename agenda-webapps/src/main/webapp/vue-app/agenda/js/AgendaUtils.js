1473c17fd80e2fdc3f52244082a5cd640b4b4e39
El kamel Hassen <helkamel@exoplatform.com>
2020-09-07 10:48:28 +0200

    add icon type for upload file

--- agenda-webapps/src/main/webapp/vue-app/agenda/js/AgendaUtils.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda/js/AgendaUtils.js
@@ -29,32 +29,6 @@ export function getWeekNumber(date) {
   return Math.ceil((firstThursday - date.valueOf()) / 604800000);
 }
 
-export function getFormattedFileSize(fileSize) {
-  let measure;
-  const BYTES_IN_KB = 1024;
-  const BYTES_IN_GB = 1073741824;
-  const MB_IN_GB = 10;
-  const BYTES_IN_MB = 1048576;
-  const formattedSizePrecision = 2;
-  const minGb = MB_IN_GB * BYTES_IN_MB; // equals 0.01 GB
-  const minMb = 10000; // equals 0.01 MB, which is the smallest number with precision `formattedSizePrecision`
-  const minKb = 10; // equals 0.01 KB, which is the smallest number with precision `formattedSizePrecision`
-  let size = fileSize;
-  if (fileSize < minKb) {
-    measure = ' bytes'; // TODO I18N
-  } else if (fileSize < minMb) {
-    size = fileSize / BYTES_IN_KB;
-    measure = ' kilo'; // TODO I18N
-  } else if (fileSize < minGb) {
-    size = fileSize / BYTES_IN_MB;
-    measure = ' mega'; // TODO I18N
-  } else {
-    size = fileSize / BYTES_IN_GB;
-    measure = ' giga'; // TODO I18N
-  }
-  return (+size).toFixed(formattedSizePrecision).concat(measure);
-}
-
 export function toVuetifyDate(date) {
   return `${date.getFullYear()}-${ 
     pad(date.getMonth() + 1)}-${ 
