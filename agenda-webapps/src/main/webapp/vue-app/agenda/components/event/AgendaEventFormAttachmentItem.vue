33e87550368021f6b65ba25775bd8c40d99bc3c6
El kamel Hassen <helkamel@exoplatform.com>
2020-09-07 10:48:28 +0200

    add icon type for upload file

--- agenda-webapps/src/main/webapp/vue-app/agenda/components/event/AgendaEventFormAttachmentItem.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda/components/event/AgendaEventFormAttachmentItem.vue
@@ -1,6 +1,7 @@
 <template>
   <v-list-item class="d-flex flex-row">
-    <div class="d-flex flex-column">
+    <i :class="getIconClassFromFileMimeType"></i>
+    <div class="d-flex flex-column ml-2">
       <a :href="attachment.url || ''" class="my-auto">{{ attachment.name }} ({{ formattedFileSize }})</a>
       <v-progress-linear
         v-if="attachment.control && uploadProgress < 100"
@@ -43,6 +44,16 @@ export default {
       } else {
         const size = Number(this.attachment.size / this.megabyte).toFixed(2);
         return this.$t('agenda.sizeInMB', {0: size});
+      }
+    },
+    getIconClassFromFileMimeType() {
+      if(this.attachment) {
+        const fileMimeTypeClass = this.attachment.mimeType.replace(/\./g, '').replace('/', '').replace('\\', '');
+        return this.attachment.mimeType
+          ? `uiIcon32x32${this.attachment.mimeType.replace(/[/.]/g, '')}`
+          : `uiIconFileType${fileMimeTypeClass} uiIconFileTypeDefault`;
+      } else {
+        return '';
       }
     },
   },
