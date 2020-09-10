e86c552d18f113a01530ed38827593f5db426fe6
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-09-10 19:16:37 +0100

    Add Drawer for choosing calendars colors

--- agenda-webapps/src/main/webapp/vue-app/agenda/js/CalendarService.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda/js/CalendarService.js
@@ -1,1 +1,30 @@
+export function getCalendars(offset, limit) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/agenda/calendars?offset=${offset}&limit=${limit}&returnSize=true`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then((resp) => {
+    if (!resp || !resp.ok) {
+      throw new Error('Error creating event');
+    } else {
+      return resp.json();
+    }
+  });
+}
 
+export function saveCalendar(calendar) {
+  calendar.owner = calendar.owner && {
+    id: calendar.owner.id,
+  };
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/agenda/calendars`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(calendar),
+  }).then((resp) => {
+    if (!resp || !resp.ok) {
+      throw new Error('Error updating calendar');
+    }
+  });
+}
