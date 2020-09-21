9adf9c64f5fac088d3ddefd1b9848b8436dd1f0a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-09-21 19:24:18 +0100

    Fix UI gliches and delete static content

--- agenda-webapps/src/main/webapp/vue-app/agenda/components/top-toolbar/AgendaSearchEvent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="uiSearchInput">
-    <input
-      id="inputValue"
-      type="text"
-      class="input-large ignore-vuetify-classes"
-      :placeholder="$t('agenda.SearchEventPlaceholder')">
-  </div>
-</template>
