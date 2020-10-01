9937fd736a5c6c268dcd2e607addfb82ad1d1501
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-01 14:17:41 +0100

    Fix computing attendee ACL of user on events

--- agenda-webapps/src/main/webapp/vue-app/agenda/components/calendar-mobile/AgendaTimeline.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda/components/calendar-mobile/AgendaTimeline.vue
@@ -1,54 +1,61 @@
 <template>
-  <v-flex v-if="events && events.length" class="agenda-timeline mt-2">
-    <v-list
-      v-for="month in eventsMonths"
-      :key="month"
-      class="agenda-timeline-month pa-0">
-      <v-list-item>
-        <v-list-item-action class="event-timeline-day" />
-        <v-list-item-action-text class="subtitle-1 mr-2">
-          <date-format :value="month" :format="monthFormat" />
-        </v-list-item-action-text>
-        <v-list-item-content>
-          <v-divider />
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item
-        v-for="eventDay in eventsDaysByMonth[month]"
-        :key="eventDay"
-        class="mb-5">
-        <v-list-item-action class="align-self-start center event-timeline-day">
-          <date-format :value="eventDay" :format="dayFormat" />
-        </v-list-item-action>
-        <v-list-item-content class="pa-0">
-          <v-list class="pa-0">
-            <v-list-item
-              v-for="(event, i) in eventsByDates[month][eventDay]"
-              :key="i"
-              :style="{background: event.color || event.calendar.color}"
-              class="event-timeline-detail d-flex flex-column white--text px-2 py-0 mb-2 border-radius"
-              dark
-              @click="$root.$emit('agenda-event-details', event)">
-              <v-list-item-content class="event-timeline-detail-content">
-                <strong class="text-truncate">{{ event.summary }}</strong>
-                <div v-if="event.allDay">
-                  {{ $t('agenda.allDay') }}
-                </div>
-                <div v-else class="d-flex flex-row">
-                  <date-format
-                    :value="event.startDate"
-                    :format="timeFormat" />
-                  <strong class="mx-2">-</strong>
-                  <date-format
-                    :value="event.endDate"
-                    :format="timeFormat" />
-                </div>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+  <v-flex class="agenda-timeline mt-2">
+    <template v-if="events && events.length">
+      <v-list
+        v-for="month in eventsMonths"
+        :key="month"
+        class="agenda-timeline-month pa-0">
+        <v-list-item>
+          <v-list-item-action class="event-timeline-day" />
+          <v-list-item-action-text class="subtitle-1 mr-2">
+            <date-format :value="month" :format="monthFormat" />
+          </v-list-item-action-text>
+          <v-list-item-content>
+            <v-divider />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          v-for="eventDay in eventsDaysByMonth[month]"
+          :key="eventDay"
+          class="mb-5">
+          <v-list-item-action class="align-self-start center event-timeline-day">
+            <date-format :value="eventDay" :format="dayFormat" />
+          </v-list-item-action>
+          <v-list-item-content class="pa-0">
+            <v-list class="pa-0">
+              <v-list-item
+                v-for="(event, i) in eventsByDates[month][eventDay]"
+                :key="i"
+                :style="{background: event.color || event.calendar.color}"
+                class="event-timeline-detail d-flex flex-column white--text px-2 py-0 mb-2 border-radius"
+                dark
+                @click="$root.$emit('agenda-event-details', event)">
+                <v-list-item-content class="event-timeline-detail-content">
+                  <strong class="text-truncate">{{ event.summary }}</strong>
+                  <div v-if="event.allDay">
+                    {{ $t('agenda.allDay') }}
+                  </div>
+                  <div v-else class="d-flex flex-row">
+                    <date-format
+                      :value="event.startDate"
+                      :format="timeFormat" />
+                    <strong class="mx-2">-</strong>
+                    <date-format
+                      :value="event.endDate"
+                      :format="timeFormat" />
+                  </div>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
+    <template v-else-if="!loading">
+      <v-alert type="info">
+        {{ $t('agenda.noEventsYet') }}
+      </v-alert>
+    </template>
   </v-flex>
 </template>
 <script>
@@ -57,6 +64,10 @@ export default {
     events: {
       type: Object,
       default: null,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
   data: () => ({
