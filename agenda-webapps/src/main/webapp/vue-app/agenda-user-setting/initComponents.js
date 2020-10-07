0ef962317af563221fb887253fb9cb2861e598a5
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-07 09:20:09 +0100

    Define a new Common JS Module for all Agenda apps This will allow to enhance retrieve resources in browser by making the reusable agenda components defined in one single AMD module

--- agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/initComponents.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/initComponents.js
@@ -1,9 +1,7 @@
 import AgendaUserSettings from './components/AgendaUserSettings.vue';
-import AgendaUserSettingDrawer from '../agenda/components/settings/AgendaUserSettingDrawer.vue';
 
 const components = {
   'agenda-user-settings': AgendaUserSettings,
-  'agenda-user-setting-drawer': AgendaUserSettingDrawer,
 };
 
 for (const key in components) {
