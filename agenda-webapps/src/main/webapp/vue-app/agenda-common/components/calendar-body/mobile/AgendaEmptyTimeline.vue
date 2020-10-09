33438147b3cdb21dcfe1bf5cc2d48d34709d6332
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-09 18:24:48 +0100

    Add Empty Agenda Timeline block

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-flex class="d-flex agenda-empty-timeline">
+    <div class="ma-auto py-5 d-flex flex-column align-center">
+      <i class="uiIconCalendarEmpty"></i>
+      <span class="title">
+        {{ $t('agenda.noEvent') }}
+      </span>
+      <span class="sub-title-1 text-sub-title">
+        {{ $t('agenda.noEventYet') }}
+      </span>
+    </div>
+  </v-flex>
+</template>
