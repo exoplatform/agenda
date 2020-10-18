ef88a36694aaf318278f446a8a4ac6809079b40f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-18 14:54:57 +0100

    Split user settings UI to sub components and fix default value

--- agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/initComponents.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/initComponents.js
@@ -1,7 +1,9 @@
 import AgendaUserSettings from './components/AgendaUserSettings.vue';
+import AgendaUserGeneralSettings from './components/AgendaUserGeneralSettings.vue';
 
 const components = {
   'agenda-user-settings': AgendaUserSettings,
+  'agenda-user-general-settings': AgendaUserGeneralSettings,
 };
 
 for (const key in components) {
