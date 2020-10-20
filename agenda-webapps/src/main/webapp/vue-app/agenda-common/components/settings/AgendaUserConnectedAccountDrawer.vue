c7a97d24e02452d76daa28f4614b9f5a4286e000
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-20 04:58:29 +0100

    Add google agenda connector

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/settings/AgendaUserConnectedAccountDrawer.vue
@@ -0,0 +1,89 @@
+<template>
+  <exo-drawer
+    ref="agendaConnectorsDrawer"
+    class="agendaConnectorsDrawer"
+    body-classes="hide-scroll decrease-z-index-more"
+    right>
+    <template slot="title">
+      {{ $t('agenda.connectYourPersonalAgenda') }}
+    </template>
+    <template slot="content">
+      <v-list two-line>
+        <v-list-item v-for="connector in connectors" :key="connector.name">
+          <v-list-item-avatar>
+            <v-avatar tile size="40">
+              <img :alt="connector.name" :src="connector.avatar">
+            </v-avatar>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <template v-if="connector.user">
+              <v-list-item-title>
+                {{ $t('agenda.connectedAccountWith') }}:
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ connector.user }}
+              </v-list-item-subtitle>
+            </template>
+            <template v-else>
+              <v-list-item-title class="title">
+                {{ $t(connector.name) }}
+              </v-list-item-title>
+            </template>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+              v-if="connector.user"
+              :loading="connector.loading"
+              class="btn"
+              @click="connector.disconnect()">
+              {{ $t('agenda.disconnect') }}
+            </v-btn>
+            <v-btn
+              v-else
+              :loading="connector.loading"
+              class="btn"
+              @click="connector.connect()">
+              {{ $t('agenda.connect') }}
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </template>
+  </exo-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    connectors: [],
+  }),
+  created() {
+    this.$root.$on('agenda-connected-account-settings-open', this.open);
+
+    // Retrieving list of registered connectors from extensionRegistry
+    document.addEventListener('agenda-accounts-connectors-refresh', this.refreshConnectorsList);
+    this.refreshConnectorsList();
+  },
+  methods: {
+    refreshConnectorsList() {
+      this.connectors = extensionRegistry.loadExtensions('agenda', 'connectors') || [];
+    },
+    open() {
+      this.connectors.forEach(connector => {
+        if (connector.init) {
+          connector.init(this.connectionStatusChanged, this.connectionLoading);
+        }
+      });
+      this.$refs.agendaConnectorsDrawer.open();
+    },
+    connectionLoading(connector, loading) {
+      this.$set(connector, 'loading', loading);
+      this.$forceUpdate();
+    },
+    connectionStatusChanged(connector, currentUser) {
+      connector.user = currentUser && currentUser.user || null;
+    },
+  },
+};
+</script>
+
--- agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/components/AgendaUserGeneralSettings.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-user-setting/components/AgendaUserGeneralSettings.vue
@@ -1,65 +1,64 @@
 <template>
-  <v-card
-    v-if="displayed"
-    class="ma-4 border-radius"
-    flat>
-    <v-list two-line>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title text-color">
-            <div :class="skeleton && 'skeleton-background skeleton-border-radius skeleton-text-width skeleton-text-height my-2'">
-              {{ skeleton && '&nbsp;' || $t('agenda') }}
-            </div>
-          </v-list-item-title>
-          <v-list-item-subtitle class="text-sub-title">
-            <div :class="skeleton && 'skeleton-background skeleton-border-radius skeleton-text-width-small skeleton-text-height-fine my-2'">
-              <v-list-item v-if="settings" dense>
-                <v-list-item-content class="pa-0">
-                  <v-list-item-title class="text-wrap">
-                    <template>
-                      <v-chip
-                        class="ma-2"
-                        color="primary">
-                        <span class="text-capitalize">{{ settings.agendaDefaultView }}</span>
-                        <span class="pl-1">{{ $t('agenda.view') }}</span>
-                      </v-chip>
-                      <v-chip
-                        class="ma-2"
-                        color="primary">
-                        {{ agendaWeekStartOnLabel }}
-                      </v-chip>
-                      <v-chip
-                        v-if="agendaWorkingTime"
-                        class="ma-2"
-                        color="primary">
-                        {{ agendaWorkingTime }}
-                      </v-chip>
-                    </template>
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </div>
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-btn
-            :class="skeleton && 'skeleton-background'"
-            small
-            icon
-            @click="openDrawer">
-            <i v-if="!skeleton" class="uiIconEdit uiIconLightBlue pb-2"></i>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title class="title text-color">
+        <div :class="skeleton && 'skeleton-background skeleton-border-radius skeleton-text-width skeleton-text-height my-2'">
+          {{ skeleton && '&nbsp;' || $t('agenda') }}
+        </div>
+      </v-list-item-title>
+      <v-list-item-subtitle class="text-sub-title">
+        <div :class="skeleton && 'skeleton-background skeleton-border-radius skeleton-text-width-small skeleton-text-height-fine my-2'">
+          <v-list-item v-if="settings" dense>
+            <v-list-item-content class="pa-0">
+              <v-list-item-title class="text-wrap">
+                <template>
+                  <v-chip
+                    class="ma-2"
+                    color="primary">
+                    <span class="text-capitalize">{{ settings.agendaDefaultView }}</span>
+                    <span class="pl-1">{{ $t('agenda.view') }}</span>
+                  </v-chip>
+                  <v-chip
+                    class="ma-2"
+                    color="primary">
+                    {{ agendaWeekStartOnLabel }}
+                  </v-chip>
+                  <v-chip
+                    v-if="agendaWorkingTime"
+                    class="ma-2"
+                    color="primary">
+                    {{ agendaWorkingTime }}
+                  </v-chip>
+                </template>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+      </v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-action>
+      <v-btn
+        :class="skeleton && 'skeleton-background'"
+        small
+        icon
+        @click="openDrawer">
+        <i v-if="!skeleton" class="uiIconEdit uiIconLightBlue pb-2"></i>
+      </v-btn>
+    </v-list-item-action>
     <agenda-user-setting-drawer
       ref="agendaDrawer"
       :settings="settings" />
-  </v-card>
+  </v-list-item>
 </template>
 
 <script>
 export default {
+  props: {
+    skeleton: {
+      type: Boolean,
+      default: true,
+    },
+  },
   data: () => ({
     id: `Settings${parseInt(Math.random() * 10000)
       .toString()
@@ -71,8 +70,6 @@ export default {
       workingTimeStart: '08:00',
       workingTimeEnd: '18:00',
     },
-    displayed: true,
-    skeleton: true,
   }),
   computed: {
     DAY_NAME_BY_ABBREVIATION () {
@@ -97,13 +94,6 @@ export default {
     }
   },
   created() {
-    document.addEventListener('hideSettingsApps', (event) => {
-      if (event && event.detail && this.id !== event.detail) {
-        this.displayed = false;
-      }
-    });
-    document.addEventListener('showSettingsApps', () => this.displayed = true);
-    this.skeleton = false;
     this.$root.$on('agenda-refresh', this.refresh);
     this.refresh();
   },
@@ -113,11 +103,7 @@ export default {
         if (settings && settings.value) {
           this.settings = JSON.parse(settings.value);
         }
-      })
-        .finally(() => {
-          document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
-          this.skeleton = false;
-        });
+      });
     },
     openDrawer(){
       this.$refs.agendaDrawer.open();
