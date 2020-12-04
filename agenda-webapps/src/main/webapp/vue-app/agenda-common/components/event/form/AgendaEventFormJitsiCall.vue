068710bd442682cb24a7e87f7e54cc820755c09e
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2020-12-04 17:08:03 +0100

    implement create jitsi button on event creation (#111)

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/form/AgendaEventFormJitsiCall.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <template v-if="!showCallUrl">
+      <v-btn
+        class="btn primary border-radius"
+        @click="generateUrl">
+        {{ $t('agenda.generate.callUrl') }}
+      </v-btn>
+    </template>
+    <template v-else>
+      <span v-autolinker="callUrl"></span>
+      <v-btn
+        title="delete call url"
+        color="grey"
+        icon
+        dark
+        class="ml-auto"
+        @click="close">
+        <v-icon>
+          mdi-close
+        </v-icon>
+      </v-btn>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    callUrl: 'https://community.exoplatform.com/jitsi/meet/g_agenda_ft_test',
+    showCallUrl: false,
+  }),
+  methods:{
+    generateUrl(){
+      if(!this.showCallUrl){
+        this.showCallUrl = true;
+      }
+    },
+    close() {
+      this.showCallUrl = false;
+    }
+  }
+};
+</script>
