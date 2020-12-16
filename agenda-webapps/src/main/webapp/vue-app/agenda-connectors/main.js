71cdc0fc37e1c572ab537aad60911d09c15255c6
aelbenney <42932906+aelbenney@users.noreply.github.com>
2020-12-16 10:44:29 +0100

    Feature/agenda: Implement Office 365 agenda connector

--- agenda-webapps/src/main/webapp/vue-app/agenda-connectors/main.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda-connectors/main.js
@@ -1,5 +1,7 @@
 import googleConnector from './agendaGoogleConnector.js';
+import officeConnector from './agendaOfficeConnector.js';
 
 extensionRegistry.registerExtension('agenda', 'connectors', googleConnector);
+extensionRegistry.registerExtension('agenda', 'connectors', officeConnector);
 
 document.dispatchEvent(new CustomEvent('agenda-connectors-refresh'));
