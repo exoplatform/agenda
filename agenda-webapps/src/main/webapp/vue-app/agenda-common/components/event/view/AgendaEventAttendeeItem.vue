dce905d9986c7037108d3ec7be620b46a040c6a3
ahmed <achakroun@exoplatform.com>
2020-12-25 17:00:17 +0100

    Task-38523 : Add external tag in agenda event and fix user popover

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventAttendeeItem.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventAttendeeItem.vue
@@ -89,8 +89,11 @@ export default {
     attendeeProfileAvatarUrl() {
       return this.attendee.identity.space ? this.attendee.identity.space.avatarUrl : this.attendee.identity.profile ? this.attendee.identity.profile.avatar : '';
     },
+    fullName() {
+      return this.attendee.identity.profile.dataEntity.external &&  this.attendee.identity.profile.dataEntity.external === 'true' ? this.attendee.identity.profile.fullname.concat(' (').concat(this.$t('agenda.label.external')).concat(')') : this.attendee.identity.profile.fullname;
+    },
     attendeeProfileDisplayName() {
-      return this.attendee.identity.space ? this.attendee.identity.space.displayName : this.attendee.identity.profile ? this.attendee.identity.profile.fullname : '';
+      return this.attendee.identity.space ? this.attendee.identity.space.displayName : this.attendee.identity.profile ? this.fullName : '';
     },
     labels() {
       return {
@@ -102,6 +105,7 @@ export default {
         StatusTitle: this.$t('profile.StatusTitle'),
         join: this.$t('space.join'),
         leave: this.$t('space.leave'),
+        External: this.$t('agenda.label.external'),
         members: this.$t('space.members'),
       };
     },
