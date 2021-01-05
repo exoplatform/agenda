1e42d65c1ff6bf03b16e82072952579c7dbae87b
El kamel Hassen <helkamel@exoplatform.com>
2021-01-05 16:21:29 +0100

    TASK-39002: implement web notification of response participant

--- agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/WebTemplateProvider.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/WebTemplateProvider.java
@@ -2,6 +2,7 @@ package org.exoplatform.agenda.notification.provider;
 
 import org.exoplatform.agenda.notification.builder.AgendaTemplateBuilder;
 import org.exoplatform.agenda.notification.builder.ReminderTemplateBuilder;
+import org.exoplatform.agenda.notification.builder.ReplyTemplateBuilder;
 import org.exoplatform.commons.api.notification.annotation.TemplateConfig;
 import org.exoplatform.commons.api.notification.annotation.TemplateConfigs;
 import org.exoplatform.commons.api.notification.channel.template.TemplateProvider;
@@ -14,7 +15,8 @@ import static org.exoplatform.agenda.util.NotificationUtils.*;
     @TemplateConfig(pluginId = AGENDA_EVENT_ADDED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventWebPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_EVENT_MODIFIED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventWebPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_EVENT_CANCELLED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventWebPlugin.gtmpl"),
-    @TemplateConfig(pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReminderWebPlugin.gtmpl") })
+    @TemplateConfig(pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReminderWebPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReplyWebPlugin.gtmpl"), })
 public class WebTemplateProvider extends TemplateProvider {
   public WebTemplateProvider(ExoContainer container, InitParams initParams) {
     super(initParams);
@@ -22,5 +24,6 @@ public class WebTemplateProvider extends TemplateProvider {
     this.templateBuilders.put(EVENT_MODIFIED_KEY, new AgendaTemplateBuilder(this, container, EVENT_MODIFIED_KEY, false));
     this.templateBuilders.put(EVENT_CANCELLED_KEY, new AgendaTemplateBuilder(this, container, EVENT_CANCELLED_KEY, false));
     this.templateBuilders.put(EVENT_REMINDER_KEY, new ReminderTemplateBuilder(this, container, EVENT_REMINDER_KEY, false));
+    this.templateBuilders.put(EVENT_REPLY_KEY, new ReplyTemplateBuilder(this, container, EVENT_REPLY_KEY, false));
   }
 }
