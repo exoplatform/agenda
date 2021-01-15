5ec333d398925ae8a70d0dc36f7cc03e584600bc
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-01-15 09:15:59 +0100

    TASK-39002: implement web notification of response participant (#165)

---
+++ agenda-services/src/test/java/org/exoplatform/agenda/listener/AgendaReplyEventNotificationListenerTest.java
@@ -0,0 +1,45 @@
+package org.exoplatform.agenda.listener;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.junit.Test;
+
+import org.exoplatform.agenda.constant.EventAttendeeResponse;
+import org.exoplatform.agenda.model.Event;
+import org.exoplatform.agenda.model.EventAttendee;
+import org.exoplatform.agenda.service.BaseAgendaEventTest;
+import org.exoplatform.commons.api.notification.model.WebNotificationFilter;
+import org.exoplatform.commons.api.notification.service.WebNotificationService;
+
+public class AgendaReplyEventNotificationListenerTest extends BaseAgendaEventTest {
+
+  @Test
+  public void testSendNotificationWhenReplyToEvent() throws Exception {
+
+    ZonedDateTime start = ZonedDateTime.now().withNano(0);
+
+    boolean allDay = false;
+
+    Event event = newEventInstance(start, start, allDay);
+    event = createEvent(event.clone(), Long.parseLong(testuser1Identity.getId()), testuser4Identity, testuser5Identity);
+
+    long eventParticipantId = Long.parseLong(testuser4Identity.getId());
+
+    WebNotificationService webNotificationService = container.getComponentInstanceOfType(WebNotificationService.class);
+    int initialNotificationsSize = webNotificationService.getNumberOnBadge(testuser1Identity.getRemoteId());
+
+    EventAttendee oldAttendeeResponse =
+                                      new EventAttendee(0, event.getId(), eventParticipantId, EventAttendeeResponse.NEEDS_ACTION);
+    EventAttendee newAttendeeResponse = new EventAttendee(0, event.getId(), eventParticipantId, EventAttendeeResponse.ACCEPTED);
+
+    AgendaEventReplyListener agendaEventReplyListener = new AgendaEventReplyListener(container);
+    agendaEventReplyListener.onEvent(new org.exoplatform.services.listener.Event<EventAttendee, EventAttendee>(null,
+                                                                                                               oldAttendeeResponse,
+                                                                                                               newAttendeeResponse));
+
+    assertEquals(initialNotificationsSize + 1, webNotificationService.getNumberOnBadge(testuser1Identity.getRemoteId()));
+  }
+}
