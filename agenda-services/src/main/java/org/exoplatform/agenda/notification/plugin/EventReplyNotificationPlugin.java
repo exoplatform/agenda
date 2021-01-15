b0739ffe781b316c0b9be76fa228eb1c352bfb2d
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-01-15 09:15:59 +0100

    TASK-39002: implement web notification of response participant (#165)

---
+++ agenda-services/src/main/java/org/exoplatform/agenda/notification/plugin/EventReplyNotificationPlugin.java
@@ -0,0 +1,75 @@
+package org.exoplatform.agenda.notification.plugin;
+
+import org.apache.commons.lang.StringUtils;
+import org.exoplatform.agenda.constant.EventAttendeeResponse;
+import org.exoplatform.agenda.model.Calendar;
+import org.exoplatform.agenda.model.Event;
+import org.exoplatform.agenda.service.AgendaCalendarService;
+import org.exoplatform.commons.api.notification.NotificationContext;
+import org.exoplatform.commons.api.notification.model.NotificationInfo;
+import org.exoplatform.commons.api.notification.plugin.BaseNotificationPlugin;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.ValueParam;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.manager.IdentityManager;
+
+import static org.exoplatform.agenda.util.NotificationUtils.*;
+import static org.exoplatform.agenda.util.NotificationUtils.storeEventParameters;
+
+public class EventReplyNotificationPlugin extends BaseNotificationPlugin {
+  private static final Log    LOG                             = ExoLogger.getLogger(EventReminderNotificationPlugin.class);
+
+  private static final String AGENDA_NOTIFICATION_PLUGIN_NAME = "agenda.notification.plugin.key";
+
+  private String              notificationId;
+
+  private IdentityManager     identityManager;
+
+  private AgendaCalendarService calendarService;
+
+  public EventReplyNotificationPlugin(InitParams initParams, IdentityManager identityManager, AgendaCalendarService calendarService) {
+    super(initParams);
+    this.identityManager = identityManager;
+    this.calendarService = calendarService;
+    ValueParam notificationIdParam = initParams.getValueParam(AGENDA_NOTIFICATION_PLUGIN_NAME);
+    if (notificationIdParam == null || StringUtils.isBlank(notificationIdParam.getValue())) {
+      throw new IllegalStateException("'agenda.notification.plugin.key' parameter is mandatory");
+    }
+    this.notificationId = notificationIdParam.getValue();
+  }
+
+  @Override
+  public String getId() {
+    return this.notificationId;
+  }
+
+  @Override
+  public boolean isValid(NotificationContext ctx) {
+    if (getEventId(ctx) == 0) {
+      LOG.warn("Notification type '{}' isn't valid because the event wasn't found", getId());
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public NotificationInfo makeNotification(NotificationContext ctx) {
+    Event event = ctx.value(EVENT_AGENDA);
+    Calendar calendar = calendarService.getCalendarById(event.getCalendarId());
+    long eventParticipantId = ctx.value(EVENT_PARTICIPANT_ID);
+    EventAttendeeResponse eventResponse = ctx.value(EVENT_RESPONSE);
+    NotificationInfo notification = NotificationInfo.instance();
+    notification.key(getId());
+    if (event.getId() > 0) {
+      setEventReminderNotificationRecipients(identityManager, notification, event.getCreatorId());
+    }
+    if (notification.getSendToUserIds() == null || notification.getSendToUserIds().isEmpty()) {
+      LOG.debug("Notification type '{}' doesn't have a recipient", getId());
+      return null;
+    } else {
+      storeEventParameters(identityManager, notification, event, eventParticipantId, eventResponse, calendar);
+      return notification.end();
+    }
+  }
+}
