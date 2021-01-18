57a3f3dd2d658b177bef72a69e30186d755e775b
El kamel Hassen <helkamel@exoplatform.com>
2021-01-18 10:02:47 +0100

    TASK-39002: fix send invitation by select all space

--- agenda-services/src/main/java/org/exoplatform/agenda/listener/AgendaEventReplyListener.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/listener/AgendaEventReplyListener.java
@@ -31,11 +31,13 @@ public class AgendaEventReplyListener extends Listener<EventAttendee, EventAtten
     try {
       EventAttendee oldAttendee = event.getSource();
       EventAttendee newAttendee = event.getData();
-      org.exoplatform.agenda.model.Event agendaEvent = getAgendaEventService().getEventById(oldAttendee.getEventId());
+      org.exoplatform.agenda.model.Event agendaEvent = getAgendaEventService().getEventById(newAttendee.getEventId());
       // Avoid notifying creator when he changes his response and avoid
       // notifying him when a user doesn't change his response
-      if (oldAttendee.getResponse() != newAttendee.getResponse() && agendaEvent.getCreatorId() != oldAttendee.getIdentityId()) {
-        sendReplyResponseNotification(agendaEvent, oldAttendee.getIdentityId(), newAttendee.getResponse());
+      if ((oldAttendee == null && agendaEvent.getCreatorId() != newAttendee.getIdentityId())
+          || (oldAttendee.getResponse() != newAttendee.getResponse()
+              && agendaEvent.getCreatorId() != oldAttendee.getIdentityId())) {
+        sendReplyResponseNotification(agendaEvent, newAttendee.getIdentityId(), newAttendee.getResponse());
       }
     } finally {
       RequestLifeCycle.end();
