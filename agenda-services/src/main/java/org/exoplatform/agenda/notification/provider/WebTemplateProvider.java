331d69d65e305cb9cbe10a5cf618a5e3a1852b96
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-01-22 18:01:16 +0100

    TASK-39622: Implement web notification for date poll (#212)

--- agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/WebTemplateProvider.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/WebTemplateProvider.java
@@ -1,6 +1,7 @@
 package org.exoplatform.agenda.notification.provider;
 
 import org.exoplatform.agenda.notification.builder.AgendaTemplateBuilder;
+import org.exoplatform.agenda.notification.builder.DatePollNotificationBuilder;
 import org.exoplatform.agenda.notification.builder.ReminderTemplateBuilder;
 import org.exoplatform.agenda.notification.builder.ReplyTemplateBuilder;
 import org.exoplatform.commons.api.notification.annotation.TemplateConfig;
@@ -16,7 +17,8 @@ import static org.exoplatform.agenda.util.NotificationUtils.*;
     @TemplateConfig(pluginId = AGENDA_EVENT_MODIFIED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventWebPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_EVENT_CANCELLED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventWebPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReminderWebPlugin.gtmpl"),
-    @TemplateConfig(pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReplyWebPlugin.gtmpl"), })
+    @TemplateConfig(pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/EventReplyWebPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_DATE_POLL_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/web/DatePollWebPlugin.gtmpl") })
 public class WebTemplateProvider extends TemplateProvider {
   public WebTemplateProvider(ExoContainer container, InitParams initParams) {
     super(initParams);
@@ -25,5 +27,6 @@ public class WebTemplateProvider extends TemplateProvider {
     this.templateBuilders.put(EVENT_CANCELLED_KEY, new AgendaTemplateBuilder(this, container, EVENT_CANCELLED_KEY, false));
     this.templateBuilders.put(EVENT_REMINDER_KEY, new ReminderTemplateBuilder(this, container, EVENT_REMINDER_KEY, false));
     this.templateBuilders.put(EVENT_REPLY_KEY, new ReplyTemplateBuilder(this, container, EVENT_REPLY_KEY, false));
+    this.templateBuilders.put(EVENT_DATE_POLL_KEY, new DatePollNotificationBuilder(this, container, EVENT_DATE_POLL_KEY, false));
   }
 }
