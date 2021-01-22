8f1ad3e4149d0f5daefae391f484b24287a7a9da
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-01-22 17:58:37 +0100

    TASK-39620: implement push notification for date poll (#210)

--- agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/MobilePushTemplateProvider.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/MobilePushTemplateProvider.java
@@ -1,6 +1,7 @@
 package org.exoplatform.agenda.notification.provider;
 
 import org.exoplatform.agenda.notification.builder.AgendaTemplateBuilder;
+import org.exoplatform.agenda.notification.builder.DatePollNotificationBuilder;
 import org.exoplatform.agenda.notification.builder.ReminderTemplateBuilder;
 import org.exoplatform.agenda.notification.builder.ReplyTemplateBuilder;
 import org.exoplatform.commons.api.notification.annotation.TemplateConfig;
@@ -11,29 +12,14 @@ import org.exoplatform.container.xml.InitParams;
 
 import static org.exoplatform.agenda.util.NotificationUtils.*;
 
-@TemplateConfigs(
-    templates = {
-        @TemplateConfig(
-            pluginId = AGENDA_EVENT_ADDED_NOTIFICATION_PLUGIN,
-            template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"
-        ),
-        @TemplateConfig(
-            pluginId = AGENDA_EVENT_MODIFIED_NOTIFICATION_PLUGIN,
-            template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"
-        ),
-        @TemplateConfig(
-            pluginId = AGENDA_EVENT_CANCELLED_NOTIFICATION_PLUGIN,
-            template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"
-        ),
-        @TemplateConfig(
-            pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN,
-            template = "war:/conf/agenda/templates/notification/push/EventReminderPushPlugin.gtmpl"
-        ),
-        @TemplateConfig(
-            pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN,
-            template = "war:/conf/agenda/templates/notification/push/EventReplyPushPlugin.gtmpl"
-        ) }
-)
+
+@TemplateConfigs(templates = {
+    @TemplateConfig(pluginId = AGENDA_EVENT_ADDED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_EVENT_MODIFIED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_EVENT_CANCELLED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/EventPushPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/EventReminderPushPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/EventReplyPushPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_DATE_POLL_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/push/DatePollPushPlugin.gtmpl"), })
 public class MobilePushTemplateProvider extends TemplateProvider {
   public MobilePushTemplateProvider(ExoContainer container, InitParams initParams) {
     super(initParams);
@@ -42,5 +28,6 @@ public class MobilePushTemplateProvider extends TemplateProvider {
     this.templateBuilders.put(EVENT_CANCELLED_KEY, new AgendaTemplateBuilder(this, container, EVENT_CANCELLED_KEY, true));
     this.templateBuilders.put(EVENT_REMINDER_KEY, new ReminderTemplateBuilder(this, container, EVENT_REMINDER_KEY, true));
     this.templateBuilders.put(EVENT_REPLY_KEY, new ReplyTemplateBuilder(this, container, EVENT_REPLY_KEY, true));
+    this.templateBuilders.put(EVENT_DATE_POLL_KEY, new DatePollNotificationBuilder(this, container, EVENT_DATE_POLL_KEY, true));
   }
 }
