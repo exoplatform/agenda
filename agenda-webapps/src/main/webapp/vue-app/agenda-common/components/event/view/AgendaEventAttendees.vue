816e11539d8182b1d480e5e1838e3f1b04d9521a
Boubaker Khanfir <bkhanfir@exoplatform.com>
2021-03-16 17:52:12 +0100

    TASK-41626 Update Event view display (#287)

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventAttendees.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventAttendees.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="event-attendees ml-5 mt-2 flex-column">
       <agenda-event-attendee-item
-        v-for="attendee in attendees"
+        v-for="attendee in displayedAttendees"
         :key="attendee"
         :attendee="attendee"
         :creator="event.creator"
@@ -25,31 +25,73 @@ export default {
   },
   computed: {
     attendees() {
-      return this.event && this.event.attendees;
+      return this.event && this.event.attendees || [];
+    },
+    creatorAttendee() {
+      if (!this.event || !this.attendees || !this.attendees.length) {
+        return null;
+      }
+      return this.attendees.find(attendee => attendee.identity.id === this.event.creator.id);
+    },
+    creatorAttendeeResponse() {
+      return this.creatorAttendee && this.creatorAttendee.response;
+    },
+    participatingAttendees() {
+      if (!this.creatorAttendee) {
+        return this.attendees;
+      }
+      return this.attendees.filter(attendee => attendee.identity.id !== this.creatorAttendee.identity.id);
+    },
+    participatingUserAttendees() {
+      return this.participatingAttendees.filter(attendee => attendee.identity.profile);
+    },
+    participatingSpaceAttendees() {
+      return this.participatingAttendees
+        .filter(attendee => attendee.identity.space)
+        .sort(this.sortAttendees);
+    },
+    acceptedResponses() {
+      return this.participatingUserAttendees
+        .filter(attendee => attendee && attendee.response === 'ACCEPTED')
+        .sort(this.sortAttendees);
+    },
+    tentativeResponses() {
+      return this.participatingUserAttendees
+        .filter(attendee => attendee && attendee.response === 'TENTATIVE')
+        .sort(this.sortAttendees);
+    },
+    refusedResponses() {
+      return this.participatingUserAttendees
+        .filter(attendee => attendee && attendee.response === 'DECLINED')
+        .sort(this.sortAttendees);
+    },
+    needsActionResponses() {
+      return this.participatingUserAttendees
+        .filter(attendee => attendee && attendee.response === 'NEEDS_ACTION')
+        .sort(this.sortAttendees);
     },
     acceptedResponsesCount() {
-      if (!this.attendees || !this.attendees.length) {
-        return 0;
-      }
-      return this.attendees.filter(attendee => attendee && attendee.response === 'ACCEPTED').length;
-    },
-    refusedResponsesCount() {
-      if (!this.attendees || !this.attendees.length) {
-        return 0;
-      }
-      return this.attendees.filter(attendee => attendee && attendee.response === 'DECLINED').length;
+      return this.acceptedResponses.length + (this.creatorAttendeeResponse === 'ACCEPTED' && 1 || 0);
     },
     tentativeResponsesCount() {
-      if (!this.attendees || !this.attendees.length) {
-        return 0;
-      }
-      return this.attendees.filter(attendee => attendee && attendee.response === 'TENTATIVE').length;
+      return this.tentativeResponses.length + (this.creatorAttendeeResponse === 'TENTATIVE' && 1 || 0);
+    },
+    refusedResponsesCount() {
+      return this.refusedResponses.length + (this.creatorAttendeeResponse === 'DECLINED' && 1 || 0);
     },
     needsActionResponsesCount() {
-      if (!this.attendees || !this.attendees.length) {
-        return 0;
-      }
-      return this.attendees.filter(attendee => attendee && attendee.response === 'NEEDS_ACTION').length;
+      return this.needsActionResponses.length + (this.creatorAttendeeResponse === 'NEEDS_ACTION' && 1 || 0);
+    },
+    displayedAttendees() {
+      const displayedAttendees = [
+        this.creatorAttendee,
+        ...this.acceptedResponses,
+        ...this.tentativeResponses,
+        ...this.refusedResponses,
+        ...this.needsActionResponses,
+        ...this.participatingSpaceAttendees
+      ];
+      return displayedAttendees;
     },
     attendeesResponsesTitle() {
       return this.$t('agenda.attendeesResponsesOverview', {
@@ -58,6 +100,15 @@ export default {
         2: this.needsActionResponsesCount,
         3: this.tentativeResponsesCount,
       });
+    },
+  },
+  methods: {
+    sortAttendees(attendee1, attendee2) {
+      const displayName1 = (attendee1.identity.profile && attendee1.identity.profile.fullname)
+        || (attendee1.identity.space && attendee1.identity.space.displayName) || '';
+      const displayName2 = (attendee2.identity.profile && attendee2.identity.profile.fullname)
+        || (attendee2.identity.space && attendee2.identity.space.displayName) || '';
+      return displayName1.localeCompare(displayName2);
     },
   },
 };
