2b8a665e1cf01f80d9a067eae6057957010ca831
Boubaker Khanfir <bkhanfir@exoplatform.com>
2021-03-16 17:52:12 +0100

    TASK-41626 Update Event view display (#287)

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventsDetailsBody.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventsDetailsBody.vue
@@ -1,127 +1,131 @@
 <template>
   <div class="event-details-body overflow-auto flex-grow-1 d-flex flex-column flex-md-row pa-4">
-    <div class="flex-grow-1 flex-shrink-0 event-details-body-left">
-      <div class="event-date align-center d-flex pb-5">
-        <i class="uiIconDatePicker darkGreyIcon uiIcon32x32 pr-5"></i>
-        <div class="d-inline-flex">
-          <date-format
-            :value="event.startDate"
-            :format="fullDateFormat"
-            class="mr-1" />
-          <template v-if="!sameDayDates">
-            -
-            <date-format
-              :value="event.endDate"
-              :format="fullDateFormat"
-              class="ml-1" />
-          </template>
-        </div>
-      </div>
-      <div class="event-time align-center d-flex pb-5">
-        <i class="uiIconClock darkGreyIcon uiIcon32x32 pr-5"></i>
-        <div class="d-inline-flex">
-          <template v-if="event.allDay">
-            {{ $t('agenda.allDay') }}
-          </template>
-          <template v-else>
+    <div class="flex-grow-1 flex-shrink-0 d-flex event-details-body-left">
+      <div class="mx-auto">
+        <div class="event-date align-center d-flex pb-5">
+          <i class="uiIconDatePicker darkGreyIcon uiIcon32x32 pr-5"></i>
+          <div class="d-inline-flex">
             <date-format
               :value="event.startDate"
-              :format="dateTimeFormat"
+              :format="fullDateFormat"
               class="mr-1" />
-            -
-            <date-format
-              :value="event.endDate"
-              :format="dateTimeFormat"
-              class="ml-1 mr-2" />
-          </template>
-          <template>
-            ( {{ timeZoneName }} )
-          </template>
+            <template v-if="!sameDayDates">
+              -
+              <date-format
+                :value="event.endDate"
+                :format="fullDateFormat"
+                class="ml-1" />
+            </template>
+          </div>
         </div>
-      </div>
-      <div v-if="hasRecurrence" class="event-recurrence align-center d-flex pl-1 pb-5 text-truncate">
-        <i class="uiIconRefresh darkGreyIcon uiIcon32x32 pr-5"></i>
-        <agenda-event-recurrence :event="event" class="text-wrap text-left" />
-      </div>
-      <div v-if="canAddReminders" class="event-reminders align-center d-flex pb-5 text-truncate">
-        <i class="uiIcon32x32 notifIcon darkGreyIcon pr-5 mt-1 mb-auto"></i>
-        <v-list
-          class="py-0 text-truncate"
-          dense>
-          <v-list-item v-if="!event.reminders || !event.reminders.length">
-            <label class="text-sub-title font-italic mx-auto">
-              {{ $t('agenda.noRemindersYet') }}
-            </label>
-          </v-list-item>
-          <template v-else>
-            <v-list-item
-              v-for="(reminder, index) in event.reminders"
-              :key="index"
-              class="pl-0"
-              dense>
-              <v-chip
-                class="mt-1 mb-2 mr-2"
-                color="primary"
-                outlined>
-                <span class="text--primary text-truncate">
-                  <template v-if="reminder.before">
-                    {{ $t('agenda.label.notifyMeBefore', {0: reminder.before, 1: $t(`agenda.option.${reminder.beforePeriodType.toLowerCase()}s`).toLowerCase()}) }}
-                  </template>
-                  <template v-else>
-                    {{ $t('agenda.label.notifyMeWhenEventStarts') }}
-                  </template>
-                </span>
-              </v-chip>
+        <div class="event-time align-center d-flex pb-5">
+          <i class="uiIconClock darkGreyIcon uiIcon32x32 pr-5"></i>
+          <div class="d-inline-flex">
+            <template v-if="event.allDay">
+              {{ $t('agenda.allDay') }}
+            </template>
+            <template v-else>
+              <date-format
+                :value="event.startDate"
+                :format="dateTimeFormat"
+                class="mr-1" />
+              -
+              <date-format
+                :value="event.endDate"
+                :format="dateTimeFormat"
+                class="ml-1 mr-2" />
+            </template>
+            <template>
+              ( {{ timeZoneName }} )
+            </template>
+          </div>
+        </div>
+        <div v-if="hasRecurrence" class="event-recurrence align-center d-flex pl-1 pb-5 text-truncate">
+          <i class="uiIconRefresh darkGreyIcon uiIcon32x32 pr-5"></i>
+          <agenda-event-recurrence :event="event" class="text-wrap text-left" />
+        </div>
+        <div v-if="canAddReminders" class="event-reminders align-center d-flex pb-5 text-truncate">
+          <i class="uiIcon32x32 notifIcon darkGreyIcon pr-5 mt-1 mb-auto"></i>
+          <v-list
+            class="py-0 text-truncate"
+            dense>
+            <v-list-item v-if="!event.reminders || !event.reminders.length">
+              <label class="text-sub-title font-italic mx-auto">
+                {{ $t('agenda.noRemindersYet') }}
+              </label>
             </v-list-item>
-          </template>
-        </v-list>
-        <v-btn
-          icon
-          transparent
-          class="mb-auto border-box-sizing"
-          @click="$refs.reminders.open()">
-          <i class="uiIconEditInfo uiIcon16x16 darkGreyIcon pt-2"></i>
-        </v-btn>
-      </div>
-      <div v-if="isConferenceEnabled" class="event-conference d-flex flex-grow-0 flex-shrink-1 pb-5">
-        <i class="uiIconVideo darkGreyIcon uiIcon32x32 pr-5"></i>
-        <span v-autolinker="eventConferenceUrl" class="align-self-center text-break"></span>
-      </div>
-      <div v-if="event.location" class="event-location d-flex flex-grow-0 flex-shrink-1 pb-5">
-        <i class="uiIconCheckin darkGreyIcon uiIcon32x32 pr-5"></i>
-        <span v-autolinker="event.location" class="align-self-center text-break"></span>
-      </div>
-      <div v-if="event.description" class="event-description d-flex flex-grow-0 flex-shrink-1 pb-5">
-        <i class="uiIconDescription darkGreyIcon uiIcon32x32 pr-5"></i>
-        <span v-autolinker:[autolinkerArgs]="event.description" class="mt-1 align-self-center text-wrap text-left text-break"></span>
-      </div>
-      <div
-        v-if="event.attachments && event.attachments.length !== 0"
-        class="event-attachments align-center d-flex pb-5">
-        <i class="uiIconAttach darkGreyIcon uiIcon32x32 pr-5"></i>
+            <template v-else>
+              <v-list-item
+                v-for="(reminder, index) in event.reminders"
+                :key="index"
+                class="pl-0"
+                dense>
+                <v-chip
+                  class="mt-1 mb-2 mr-2"
+                  color="primary"
+                  outlined>
+                  <span class="text--primary text-truncate">
+                    <template v-if="reminder.before">
+                      {{ $t('agenda.label.notifyMeBefore', {0: reminder.before, 1: $t(`agenda.option.${reminder.beforePeriodType.toLowerCase()}s`).toLowerCase()}) }}
+                    </template>
+                    <template v-else>
+                      {{ $t('agenda.label.notifyMeWhenEventStarts') }}
+                    </template>
+                  </span>
+                </v-chip>
+              </v-list-item>
+            </template>
+          </v-list>
+          <v-btn
+            icon
+            transparent
+            class="mb-auto border-box-sizing"
+            @click="$refs.reminders.open()">
+            <i class="uiIconEditInfo uiIcon16x16 darkGreyIcon pt-2"></i>
+          </v-btn>
+        </div>
+        <div v-if="isConferenceEnabled" class="event-conference d-flex flex-grow-0 flex-shrink-1 pb-5">
+          <i class="uiIconVideo darkGreyIcon uiIcon32x32 pr-5"></i>
+          <span v-autolinker="eventConferenceUrl" class="align-self-center text-break"></span>
+        </div>
+        <div v-if="event.location" class="event-location d-flex flex-grow-0 flex-shrink-1 pb-5">
+          <i class="uiIconCheckin darkGreyIcon uiIcon32x32 pr-5"></i>
+          <span v-autolinker="event.location" class="align-self-center text-break"></span>
+        </div>
+        <div v-if="event.description" class="event-description d-flex flex-grow-0 flex-shrink-1 pb-5">
+          <i class="uiIconDescription darkGreyIcon uiIcon32x32 pr-5"></i>
+          <span v-autolinker:[autolinkerArgs]="event.description" class="mt-1 align-self-center text-wrap text-left text-break"></span>
+        </div>
         <div
-          v-for="attachedFile in event.attachments"
-          :key="attachedFile.name"
-          class="uploadedFilesItem">
-          <div class="showFile">
-            <exo-attachment-item :file="attachedFile" />
+          v-if="event.attachments && event.attachments.length !== 0"
+          class="event-attachments align-center d-flex pb-5">
+          <i class="uiIconAttach darkGreyIcon uiIcon32x32 pr-5"></i>
+          <div
+            v-for="attachedFile in event.attachments"
+            :key="attachedFile.name"
+            class="uploadedFilesItem">
+            <div class="showFile">
+              <exo-attachment-item :file="attachedFile" />
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div class="flex-grow-0 mx-5 d-none d-md-block">
-      <v-divider vertical />
+      <v-divider vertical class="event-details-body-divider" />
     </div>
-    <div class="flex-grow-1 flex-shrink-0 event-details-body-right">
-      <agenda-event-attendees
-        ref="agendaAttendees"
-        :event="event" />
-      <agenda-connector-contemporary-events
-        v-if="isAcceptedEvent"
-        :settings="settings"
-        :event="event"
-        :connectors="connectors"
-        class="mt-5" />
+    <div class="flex-grow-1 flex-shrink-0 d-flex event-details-body-right">
+      <div class="mx-auto">
+        <agenda-event-attendees
+          ref="agendaAttendees"
+          :event="event" />
+        <agenda-connector-contemporary-events
+          :settings="settings"
+          :event="event"
+          :connectors="connectors"
+          :class="!isAcceptedEvent && 'agenda-hidden-connectors'"
+          class="mt-5" />
+      </div>
     </div>
     <agenda-event-reminder-drawer
       v-if="canAddReminders"
