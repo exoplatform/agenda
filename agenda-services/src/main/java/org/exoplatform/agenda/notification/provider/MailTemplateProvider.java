eb433c24850850d9aa3a996e6d65776066c97338
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-03-19 11:07:14 +0100

    TASK-41962: Missing notification channels (#296)

--- agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/MailTemplateProvider.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/notification/provider/MailTemplateProvider.java
@@ -12,6 +12,7 @@ import static org.exoplatform.agenda.util.NotificationUtils.*;
 @TemplateConfigs(templates = {
     @TemplateConfig(pluginId = AGENDA_EVENT_ADDED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/EventMailPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_EVENT_MODIFIED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/EventMailPlugin.gtmpl"),
+    @TemplateConfig(pluginId = AGENDA_EVENT_CANCELLED_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/EventMailPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_REMINDER_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/EventReminderMailPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_REPLY_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/EventReplyMailPlugin.gtmpl"),
     @TemplateConfig(pluginId = AGENDA_DATE_POLL_NOTIFICATION_PLUGIN, template = "war:/conf/agenda/templates/notification/mail/DatePollMailPlugin.gtmpl"),
@@ -21,6 +22,7 @@ public class MailTemplateProvider extends TemplateProvider {
     super(initParams);
     this.templateBuilders.put(EVENT_ADDED_KEY, new AgendaTemplateBuilder(this, container, EVENT_ADDED_KEY, false));
     this.templateBuilders.put(EVENT_MODIFIED_KEY, new AgendaTemplateBuilder(this, container, EVENT_MODIFIED_KEY, false));
+    this.templateBuilders.put(EVENT_CANCELLED_KEY, new AgendaTemplateBuilder(this, container, EVENT_CANCELLED_KEY, false));
     this.templateBuilders.put(EVENT_REMINDER_KEY, new ReminderTemplateBuilder(this, container, EVENT_REMINDER_KEY, false));
     this.templateBuilders.put(EVENT_REPLY_KEY, new ReplyTemplateBuilder(this, container, EVENT_REPLY_KEY, false));
     this.templateBuilders.put(EVENT_DATE_POLL_KEY, new DatePollNotificationBuilder(this, container, EVENT_DATE_POLL_KEY, false));
