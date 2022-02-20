7e3b1395d14ad402341adad094dd2bc7b760cdef
Noussairhamrit <noussair.hamrit@esprit.tn>
2022-02-20 20:57:09 +0100

    Task-54003 : Composed last name truncated in agenda notifications
    ISSUES :  Composed last name truncated in agenda notifications FIX : to solve this issue  I modified in the function 'getEventNotificationCreatorOrModifierUserName' exactly in the logic of splite of fullName because this function take into consideration only the fullName compser by two words

--- agenda-services/src/main/java/org/exoplatform/agenda/util/NotificationUtils.java
+++ agenda-services/src/main/java/org/exoplatform/agenda/util/NotificationUtils.java
@@ -802,7 +802,11 @@ public class NotificationUtils {
 
   private static final String getEventNotificationCreatorOrModifierUserName(Identity identity) {
     String[] splited = identity.getProfile().getFullName().split(" ");
-    String fullName = StringUtils.capitalize(splited[0]).concat(" ").concat(StringUtils.capitalize(splited[1]));
+    String fullName = "";
+    for (int i=0;i<splited.length;i++){
+        fullName += StringUtils.capitalize(splited[i]).concat(" ");
+    }
+    fullName=fullName.substring(0,fullName.length()-1);
     if(Utils.isExternal(identity.getRemoteId())) {
       fullName += " " + "(" + Utils.getResourceBundleLabel(new Locale(Utils.getUserLanguage(identity.getRemoteId())), "external.label.tag") + ")";
     }
