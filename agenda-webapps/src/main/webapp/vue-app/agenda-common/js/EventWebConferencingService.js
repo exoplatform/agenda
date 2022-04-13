572968f8c39b2ff766884c6c7640116a2aba2623
Montassar-Hamadi <92787967+Montassar-Hamadi@users.noreply.github.com>
2022-04-13 09:08:22 +0100

    Task-55604: Event creation link on agenda (add inviteId). (#407)
    Problem: when editing a webConference event and save it ,a link without the parameter invited was generated.
    Fix : get the old value of conference url when the returned  attribute inviteId is null.

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/js/EventWebConferencingService.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/js/EventWebConferencingService.js
@@ -107,7 +107,7 @@ function updateConference(event, conference) {
       });
     })
     .then(callDetails => {
-      conference.url = callDetails.url;
+      conference.url = callDetails.inviteId ? `${callDetails.url}?inviteId=${callDetails.inviteId}` : conference.url;
       return conference;
     })
     .catch(() => createConference(event, conference));
