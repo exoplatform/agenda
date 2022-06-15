5b1173ee8dfff1093a4f473997834f48e32d4e77
Ahmed Ben Mbarek <92788696+abenmbarek@users.noreply.github.com>
2022-06-15 12:14:28 +0100

    Task-58284: Remove Exchange agenda connectors from the agenda codebase (#457)
    Prior to these changes, Microsoft Exchange connector is registered and managed using the same module.
    After these changes, we will be able to manage this connector (enable/disable) in a separative way and reduce the strong coupling between them.

--- agenda-webapps/src/main/webapp/vue-app/exchange-connector/main.js
+++
@@ -1,5 +0,0 @@
-import exchangeConnector from './agendaExchangeConnector.js';
-
-extensionRegistry.registerExtension('agenda', 'connectors', exchangeConnector);
-
-document.dispatchEvent(new CustomEvent('agenda-connectors-refresh'));
