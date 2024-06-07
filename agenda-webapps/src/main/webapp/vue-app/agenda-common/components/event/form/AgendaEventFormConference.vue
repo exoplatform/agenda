213e218593aa5c53774cb58941b11f815b460fd7
Romain Dénarié <romain.denarie@exoplatform.com>
2024-06-07 15:58:36 +0200

    fix: Improve Add a visioconference link with external video conference - EXO-70996
    Before this fix, when the provider allow to modify the url of the visio, the url cannot be changed
    This commit add the possibility to update the visio url

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/form/AgendaEventFormConference.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/form/AgendaEventFormConference.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="d-flex flex-row">
-    <div v-if="isConferenceEnabled" class="d-flex flex-row my-auto">
+    <v-icon :class="`darkGreyIcon my-auto ${this.iconClass}`" size="32px">fa-video</v-icon>
+    <template v-if="isConferenceEnabled">
       <template v-if="eventConference">
-        <div
-          v-if="eventConferenceUrl"
+        <input
+            id="eventCallURL"
+            ref="eventCallURL"
+            v-model="conferenceURL"
+            :placeholder="$t('agenda.webConferenceURL')"
+            type="text"
+            name="webConferenceEvent"
+            class="my-3 ignore-vuetify-classes webconference-event-input"
+            v-if="eventConferenceUrl && this.conferenceProvider.canModifyEventUrl">
+        <span
+          v-else-if="eventConferenceUrl"
           v-autolinker="eventConferenceUrl"
-          class="my-auto"></div>
+          class="my-auto"></span>
         <v-chip
           v-else
           color="primary"
-          class="my-auto"
+          class="my-auto my-3"
           outlined>
           <span class="primary--text">
             {{ $t('agenda.webConferenceScheduled') }}
@@ -21,7 +31,7 @@
           color="grey"
           icon
           dark
-          class="ms-auto"
+          class="my-3"
           @click="deleteCallURL">
           <v-icon>
             mdi-close
@@ -31,21 +41,21 @@
       <v-btn
         v-else
         :loading="loading"
-        class="btn btn-primary border-radius my-auto"
+        class="btn btn-primary border-radius my-3"
         @click="createCallUrl">
         {{ $t('agenda.createEventConference') }}
       </v-btn>
-    </div>
-    <div v-else class="d-flex flex-row my-auto">
+    </template>
+    <template v-else>
       <input
         id="eventCallURL"
         ref="eventCallURL"
         v-model="conferenceURL"
         :placeholder="$t('agenda.webConferenceURL')"
         type="text"
-        name="locationEvent"
-        class="ignore-vuetify-classes my-3 location-event-input">
-    </div>
+        name="webConferenceEvent"
+        class="my-3 ignore-vuetify-classes webconference-event-input">
+    </template>
   </div>
 </template>
 
@@ -68,6 +78,10 @@ export default {
       type: Object,
       default: () => null
     },
+    iconClass: {
+      type: Object,
+      default: () => null,
+    },
   },
   data: () => ({
     loading: false,
@@ -75,7 +89,7 @@ export default {
   }),
   computed: {
     isConferenceEnabled() {
-      return this.conferenceProvider && (!this.eventConferenceType || this.conferenceProvider.getType() === this.eventConferenceType);
+      return this.conferenceProvider && (!this.eventConferenceType || this.conferenceProvider.getType() === this.eventConferenceType || this.eventConferenceType === 'manual');
     },
     eventConferences() {
       return this.event && this.event.conferences;
@@ -109,7 +123,7 @@ export default {
     }
   },
   mounted() {
-    if (this.isConferenceEnabled && this.event && this.event.conferences && this.event.conferences.length) {
+    if (this.event && this.event.conferences && this.event.conferences.length) {
       this.conferenceURL = this.event.conferences[0].url;
     } else {
       this.conferenceURL = null;
@@ -121,9 +135,17 @@ export default {
       this.$set(this.event, 'conferences', [{
         type: this.conferenceProvider.getType(),
       }]);
+
+      if (this.conferenceProvider.canModifyEventUrl) {
+        this.conferenceProvider.getCallUrl(eXo.env.portal.spaceName).then(url => {
+          this.conferenceURL = url;
+          this.$set(this.event.conferences[0], 'url', url);
+        });
+      }
     },
     deleteCallURL() {
       this.$set(this.event, 'conferences', null);
+      this.conferenceURL = null;
     },
   }
 };
