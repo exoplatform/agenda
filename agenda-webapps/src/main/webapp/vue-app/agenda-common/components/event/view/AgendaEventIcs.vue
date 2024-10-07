cc8854a2d087e079b5b4c17d2b6b57abe638e997
Ali HAMDI <ahamdi@exoplatform.com>
2024-10-07 12:12:32 +0100

    fix: Add tooltip for new buttons - EXO-73528 (#706)

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventIcs.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/view/AgendaEventIcs.vue
@@ -3,6 +3,8 @@
     <div class="d-flex justify-center">
       <v-btn outlined color="primary"
         class="pl-2 pr-2 text-body-1 btn border-radius v-chip v-chip--outlined theme--light primary primary--text mt-4 mb-2"
+        :aria-label="$t('agenda.icsbutton')"
+        :title="$t('agenda.icsbutton')"
         @click="downloadICS">
         <v-icon class="uiIcon20x20 pe-3">
           fa-calendar-plus
