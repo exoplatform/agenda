a1a67d8cc7124301c1ebd15867df32b8e1c961ed
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-03-24 13:30:30 +0100

    fix: Unify agenda timeline portlet's placeholder with ui guidelines - EXO-77735 (#755)
    Prior to this change agenda timeline portlet's placeholder was not unified with the ui guidelines, this change is going to unify agenda timeline portlet's placeholder with ui guidelines.

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
@@ -1,13 +1,8 @@
 <template>
   <v-flex class="d-flex agenda-empty-timeline">
     <div class="ma-auto py-5 d-flex flex-column align-center">
-      <i class="uiIconCalendarEmpty"></i>
-      <span class="text-title mt-5">
-        {{ $t('agenda.noEvent') }}
-      </span>
-      <span class="text-subtitle">
-        {{ $t('agenda.noEventYet') }}
-      </span>
+      <v-icon color="secondary" size="60" class="mb-3">fas fa-calendar</v-icon>
+      <p class="mb-5 subtitle-1 text-sub-title">{{ $t('agenda.label.start.adding.events') }}</p>
     </div>
   </v-flex>
 </template>
