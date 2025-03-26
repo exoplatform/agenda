346f79ff64383c907686fd5a3be08b781f568a78
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-03-26 14:12:43 +0100

    fix: Fix alignement of agenda timeline portlet's placeholder - EXO-77735 (#762)

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-flex class="d-flex agenda-empty-timeline">
+  <v-flex class="d-flex flex-column flex-grow-1 justify-center align-center fill-height agenda-empty-timeline">
     <div class="ma-auto py-5 d-flex flex-column align-center">
       <v-icon color="secondary" size="60" class="mb-3">fas fa-calendar</v-icon>
       <p class="mb-5 subtitle-1 text-sub-title">{{ $t('agenda.label.start.adding.events') }}</p>
