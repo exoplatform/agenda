aacf9cf78fd0bd8d27ba0d2e541a5b99cdf53305
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-12-05 18:02:27 +0100

    Feat: eXIP30_US05: Display mode selector- EXO-82874 (#828)
    This commit reviews the Period selector according to the design

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/top-toolbar/AgendaSwitchView.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/top-toolbar/AgendaSwitchView.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-tabs
-    v-model="calendarType"
-    align-with-title
-    centered
-    class="text-capitalize">
-    <v-tab href="#day" dark>
-      {{ $t('agenda.label.viewDay') }}
-    </v-tab>
-    <v-tab href="#week" dark>
-      {{ $t('agenda.label.viewWeek') }}
-    </v-tab>
-    <v-tab href="#month" dark>
-      {{ $t('agenda.label.viewMonth') }}
-    </v-tab>
-  </v-tabs>
+  <div class="d-flex ms-5 my-auto">
+    <v-menu v-model="menu" offset-y>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          id="agendaDisplayOptions"
+          elevation="0"
+          class="px-0"
+          v-bind="attrs"
+          v-on="on">
+          <v-icon
+            v-if="selectedDispalyOption"
+            :class="selectedDispalyOption.icon"
+            class="icon-default-color"
+            size="20" />
+          <v-icon class="ms-1" size="13">mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list class="pa-0">
+        <v-list-item
+          v-for="item in dispalyOptions"
+          :key="item.value"
+          dense
+          @click="setDisplayOption(item)">
+          <v-list-item-icon class="me-2 my-0 align-self-center">
+            <v-icon
+              :class="[item.icon, item.value === viewType ? 'primary--text' : 'icon-default-color']"
+              size="16" />
+          </v-list-item-icon>
+          <div :class="item.value === viewType && 'primary--text'">{{ item.label }}</div>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 <script>
 export default {
@@ -23,12 +41,28 @@ export default {
       default: null
     },
   },
-  watch: {
-    calendarType(newValue, oldValue) {
-      if (oldValue) {
-        this.$root.$emit('agenda-change-period-type', this.calendarType);
-      }
+  data: () => ({
+    menu: false,
+  }),
+  computed: {
+    dispalyOptions() {
+      return [
+        { value: 'day', icon: 'fas fa-calendar-day', label: this.$t('agenda.label.viewDay') },
+        { value: 'week', icon: 'fas fa-calendar-week', label: this.$t('agenda.label.viewWeek') },
+        { value: 'month', icon: 'fas fa-calendar-alt', label: this.$t('agenda.label.viewMonth') },
+      ];
+    },
+    selectedDispalyOption() {
+      const item = this.dispalyOptions.find(i => i.value === this.calendarType);
+      return item ? item : null;
     },
   },
+  methods: {
+    setDisplayOption(item) {
+      this.calendarType = item.value;
+      this.$root.$emit('agenda-change-period-type', this.calendarType);
+      this.menu = false;
+    },
+  }
 };
 </script>
