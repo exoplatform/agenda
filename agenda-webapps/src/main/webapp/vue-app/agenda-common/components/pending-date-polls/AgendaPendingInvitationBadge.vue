98ddb326701d96dd6812a11a6548b36bd6b391f2
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-12-11 10:13:39 +0100

    Feat: eXIP30_US02: Waiting invitation button review- EXO-82848
    This commit review the Waiting invitation button according the design

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/pending-date-polls/AgendaPendingInvitationBadge.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/pending-date-polls/AgendaPendingInvitationBadge.vue
@@ -4,7 +4,7 @@
     icon
     max-width="36"
     max-height="36"
-    class="me-3"
+    class="me-2"
     @click="openPendingInvitationsDrawer()">
     <v-badge
       :value="pendingInvitationsCount"
