b9af08a1365368a2ed68504db855185ffbfe99a2
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-01-07 18:00:40 +0100

    feat: Display week-end filter - EXO-83314 - exoplatform/eXIP#30
    This commit adds an option to  display the whole week temporary to the agenda filter drawer .

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/filter/AgendaFilterCalendarDrawer.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/filter/AgendaFilterCalendarDrawer.vue
@@ -8,19 +8,34 @@
       {{ $t('agenda.filterAgendaTitle') }}
     </template>
     <template slot="content">
-      <div class="radio-group-container ps-4">
-        <v-radio-group
-          v-model="eventType">
-          <v-radio
-            :label="$t('agenda.myEvent')"
-            value="myEvent" />
-          <v-radio
-            :label="$t('agenda.declinedEvent')"
-            value="declinedEvent" />
-          <v-radio
-            :label="$t('agenda.allEvent')"
-            value="allEvent" />
-        </v-radio-group>
+      <div class="d-flex flex-column mx-4 mt-4">
+        <div class="font-weight-bold">{{ $t('agenda.filter.label.displayedEvents') }}</div>
+        <div class="radio-group-container mt-n2 ms-n1">
+          <v-radio-group
+            v-model="eventType">
+            <v-radio
+              :label="$t('agenda.myEvent')"
+              value="myEvent" />
+            <v-radio
+              :label="$t('agenda.declinedEvent')"
+              value="declinedEvent" />
+            <v-radio
+              :label="$t('agenda.allEvent')"
+              value="allEvent" />
+          </v-radio-group>
+        </div>
+      </div>
+      <div class="d-flex flex-column mx-4 my-1">
+        <div class="font-weight-bold">{{ $t('agenda.filter.label.advancedOptions') }}</div>
+        <div class="d-flex mt-1" :title="displayWholeWeekTooltip">
+          <v-checkbox
+            class="my-auto ms-n1"
+            ripple="false"
+            :disabled="defaultShowWholeWeek"
+            dense
+            v-model="showWholeWeek" />
+          <label class="switch-label-text text-subtitle-1 my-auto">{{ $t('agenda.filter.label.displayWholeWeek') }}</label>
+        </div>
       </div>
     </template>
     <template slot="footer">
@@ -38,7 +53,7 @@
         </v-btn>
         <v-spacer />
         <v-btn
-          @click="close"
+          @click="cancel"
           class="btn me-2">
           {{ $t('agenda.button.cancel') }}
         </v-btn>        
@@ -57,16 +72,33 @@ export default {
     drawer: false,
     eventType: '',
     currentSpace: '',
+    defaultShowWholeWeek: null,
+    showWholeWeek: true,
+    lastShowWholeWeek: true
   }),
   created() {
     this.$root.$on('agenda-filter-drawer-open', this.open);
   },
+  computed: {
+    displayWholeWeekTooltip() {
+      if (this.defaultShowWholeWeek) {
+        return this.$t('agenda.filter.disabledDisplayWholeWeekTooltip');
+      }
+      return this.$t('agenda.filter.displayWholeWeekTooltip');
+    },
+  },
   methods: {
-    close() {
+    cancel() {
+      this.showWholeWeek = this.lastShowWholeWeek;
       this.$refs.calendarFilters.close();
     },
-    open(currentSpace) {
+    open(currentSpace,settings) {
       this.currentSpace = currentSpace;
+      if (this.defaultShowWholeWeek === null){
+        this.defaultShowWholeWeek = !settings.showWorkingTime;
+        this.showWholeWeek = !settings.showWorkingTime;
+      }
+      this.lastShowWholeWeek = this.showWholeWeek;
       if (!this.eventType){
         this.eventType = currentSpace ? 'allEvent' : 'myEvent';
       }
@@ -74,10 +106,12 @@ export default {
     },
     confirm() {
       this.$root.$emit('agenda-event-type-changed', this.eventType);
-      this.close();
+      this.$root.$emit('agenda-show-working-changed', !this.showWholeWeek);
+      this.$refs.calendarFilters.close();
     },
     init() {
       this.eventType = this.currentSpace ? 'allEvent' : 'myEvent';
+      this.showWholeWeek = this.defaultShowWholeWeek;
       this.confirm();
     },
   },
