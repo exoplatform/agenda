0bb763c1ef3076a301f0ba72259a7e91c813aba0
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-01-28 18:18:13 +0100

    feat: Connect remote agenda in Agenda timeline porlet - EXO-84035 - exoplatform/eXIP#31
    This commit adds the botton to connect remote calendar on timeline app.

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/top-toolbar/AgendaConnectToRemote.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    v-if="connectors.length > 0 && !connectedConnector"
+    class="d-flex align-center">
+    <v-btn
+      :title="$t('agenda.button.openConnectionDrawer')"
+      icon
+      :max-width="width"
+      :max-height="height" 
+      @click="openPersonalCalendarDrawer">
+      <v-icon :size="size" class="text-light-color">
+        fas fa-plug
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '18'
+    },
+    height: {
+      type: String,
+      default: '28'
+    },
+    width: {
+      type: String,
+      default: '28'
+    },
+    connectors: {
+      type: Array,
+      default: () => null,
+    },
+  },
+
+  computed: {
+    connectedConnector() {
+      return this.connectors && this.connectors.find(connector => connector.connected);
+    },
+  },
+
+  methods: {
+    openPersonalCalendarDrawer() {
+      this.$root.$emit('agenda-connectors-drawer-open');
+    },
+  },
+};
+</script>
