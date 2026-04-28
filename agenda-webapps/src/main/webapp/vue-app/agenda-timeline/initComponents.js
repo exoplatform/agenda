6127f5f0142947ec79f887ff06dca1df79d075f7
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-04-28 17:56:37 +0100

    feat: Agenda timeline settings action and source option - EXO-84367 - exoplatform/eXIP#37 This commit adds the timeline settings drawer to define the agenda source and selected spaces list.

--- agenda-webapps/src/main/webapp/vue-app/agenda-timeline/initComponents.js
+++ agenda-webapps/src/main/webapp/vue-app/agenda-timeline/initComponents.js
@@ -1,9 +1,11 @@
 import AgendaTimelineWidget from './components/AgendaTimelineWidget.vue';
 import AgendaTimelineHeader from './components/AgendaTimelineHeader.vue';
+import AgendaTimelineSettingsDrawer from './components/AgendaTimelineSettingsDrawer.vue';
 
 const components = {
   'agenda-timeline-widget': AgendaTimelineWidget,
   'agenda-timeline-header': AgendaTimelineHeader,
+  'agenda-timeline-settings-drawer': AgendaTimelineSettingsDrawer,
 };
 
 for (const key in components) {
