e6cd7ec52c5a68f47d3e2111f6d22f37d7776c94
Ali HAMDI <ahamdi@exoplatform.com>
2026-05-13 11:58:08 +0100

    fix: period selector has a null end date - EXO-85744 (#905)
    When loading agenda application, the displayed period is refreshed while it is still initializing and does not have an end date. This causes error while retrieving events from external connectors.
    the fix waits that the agenda is fully loaded before querying the agenda external connectors

--- agenda-webapps/webpack.watch.js
+++ agenda-webapps/webpack.watch.js
@@ -5,8 +5,10 @@ const webpackProductionConfig = require('./webpack.prod.js');
 
 module.exports = merge(webpackProductionConfig, {
   mode: 'development',
+  devtool: 'eval-source-map',
   output: {
     path: '/exo-server/webapps/agenda/',
-    filename: 'js/[name].bundle.js'
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
   }
 });
