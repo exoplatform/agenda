096d039b73e42f62898c5e8fbe9f1d91d06e8d93
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-06-03 09:32:18 +0100

    fix:  Default value for see more in agenda timeline - EXO-87079
    Prior to this fix, when adding the agenda timeline, there was no default value for the see more option.
    This commit activates the see more option by default.

--- agenda-webapps/src/main/webapp/vue-app/agenda-timeline/components/AgendaTimelineSettingsDrawer.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-timeline/components/AgendaTimelineSettingsDrawer.vue
@@ -193,17 +193,34 @@ export default {
     transUpdated: false,
     currentTranslations: [],
     timelineSettings: {
-      agendaSource: 'allUsersSpaces',
       selectedSpaces: [],
+      agendaSource: 'allUsersSpaces',
       customHeader: false,
+      displaySeeMore: true,
+      seeMoreUrl: '',
+      displayPending: true,
+      displayAddEvent: true,
+      agendaFilter: 'allEvents',
+      itemsNumber: 10
+    },
+    defaultTimelineSettings: {
+      selectedSpaces: [],
+      agendaSource: 'allUsersSpaces',
+      customHeader: false,
+      displaySeeMore: true,
+      seeMoreUrl: '',
+      displayPending: true,
+      displayAddEvent: true,
+      agendaFilter: 'allEvents',
+      itemsNumber: 10
     },
   }),
   computed: {
     disabled() {
-      return (this.timelineSettings.displaySeeMore && !this.isValidLink) || !this.modified;
+      return (this.timelineSettings.displaySeeMore && this.timelineSettings.seeMoreUrl !=='' && !this.isValidLink && (this.timelineSettings.agendaSource === 'selectedSpaces' ? this.timelineSettings.selectedSpaces.length > 0 : true) )  || !this.modified;
     },
     modified() {
-      return JSON.stringify(this.timelineSettings) !== JSON.stringify(this.$root.timelineSettings) && (this.timelineSettings.agendaSource === 'selectedSpaces' ? this.timelineSettings.selectedSpaces.length > 0 : true) || this.transUpdated;
+      return JSON.stringify(this.timelineSettings) !== JSON.stringify(this.defaultTimelineSettings) || this.transUpdated;
     },
     showSuggester() {
       return this.timelineSettings.agendaSource === 'selectedSpaces';
@@ -268,6 +285,7 @@ export default {
                   },
                 };
                 this.timelineSettings.selectedSpaces = [space];
+                this.defaultTimelineSettings.selectedSpaces = [space];
               }
             });
         }  else {
@@ -283,6 +301,10 @@ export default {
       if (!this.timelineSettings.itemsNumber) {
         this.timelineSettings.itemsNumber = 10;
       }
+      if (!this.timelineSettings.agendaFilter) {
+        this.timelineSettings.agendaFilter = 'allEvents';
+      }
+      this.defaultTimelineSettings = JSON.parse(JSON.stringify(this.timelineSettings));
       this.$refs.drawer.open();
     },
     close() {
