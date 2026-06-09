5d5d54c6d483fa451792c4077c085ac2393d0cd2
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-06-09 09:30:47 +0100

    fix: Agenda timeline - Behavior when no event displayed - EXO-87083
    Prior to this fix,In the agenda timeline portlet, when there is no events, the + button is displayed and a placeholder with a label which is not the same as other portlets.
    This commit hides the + button in the header and replace the label Add event by a CTA "add event".

--- agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/calendar-body/mobile/AgendaEmptyTimeline.vue
@@ -1,13 +1,51 @@
 <template>
-  <v-sheet class="d-flex flex-column justify-center align-center fill-height z-index-one" max-height="100%">
+  <v-sheet class="d-flex flex-column justify-center align-center fill-height z-index-one pa-5" max-height="100%">
     <v-icon
       color="secondary"
       size="60"
       class="mb-2">
       fas fa-calendar
     </v-icon>
-    <p class="mt-2 mb-0 text-sub-title">
-      {{ $t('agenda.label.start.adding.events') }}
-    </p>
+    <div class="d-flex flex-grow-1 my-3 align-center justify-center">
+      <v-btn
+        v-if="displayAddEvent"
+        class="btn btn-primary"
+        outlined
+        @click="openEventForm">
+        {{ $t('agenda.title.addEvent') }}
+      </v-btn>
+    </div>
   </v-sheet>
 </template>
+<script>
+export default {
+  props: {
+    canCreateEvent: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    displayAddEvent() {
+      return this.canCreateEvent &&  this.$root.timelineSettings.displayAddEvent !== false;
+    },
+  },  
+
+  methods: {
+    openEventForm() {
+      this.$root.$emit('agenda-event-quick-form', {
+        summary: '',
+        startDate: new Date(),
+        endDate: new Date(),
+        allDay: false,
+        calendar: {
+          owner: {},
+        },
+        reminders: [],
+        attachments: [],
+        attendees: [],
+      });
+    },
+  },
+};
+</script>
