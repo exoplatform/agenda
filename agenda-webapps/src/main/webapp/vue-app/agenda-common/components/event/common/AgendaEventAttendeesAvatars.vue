cdc9e3d2c79a4064929fbac49243d5c6ed74332c
Anwar khanfir <akhanfir@exoplatform.com>
2026-07-06 15:14:58 +0100

    feat: Manage participation while viewing and editing - EXO-88080 Allow participants to be added or removed directly from both view and edit modes. A new shared avatar strip component handles display consistently across both modes, with space avatars rendered as squares and a See more hover action to open the full participants drawer.

---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/common/AgendaEventAttendeesAvatars.vue
@@ -0,0 +1,88 @@
+<template>
+   <v-card
+    :min-width="avatarsWidth"
+    class="transparent d-flex align-center"
+    flat
+    tabindex="0"
+    min-height="42"
+    @mouseenter="showSeeMore = true"
+    @mouseleave="showSeeMore = false"
+    @focusin="showSeeMore = true"
+    @focusout="showSeeMore = false">
+    <div v-if="!showSeeMore" class="d-flex align-center">
+      <v-avatar
+        v-for="(attendee, index) in visibleAttendees"
+        :key="attendee.identity.id || attendee.identity.remoteId"
+        :tile="attendee.identity.providerId === 'space'"
+        :class="[index > 0 && 'ms-n3', attendee.identity.providerId === 'space' && 'rounded']"
+        :size="size"
+        class="border-white">
+        <v-img 
+          :src="attendeeAvatarUrl(attendee)" 
+          :lazy-src="defaultAvatarUrl" 
+          eager />
+      </v-avatar>
+      <v-avatar
+        v-if="overflowCount > 0"
+        :size="size"
+        class="ms-n3 grey-lighten1-background border-white">
+        <span class="text-body white--text text-center">+{{ overflowCount }}</span>
+      </v-avatar>
+    </div>
+    <v-btn
+      v-else
+      :min-width="avatarsWidth"
+      :min-height="size"
+      class="caption white--text grey-lighten1-background px-1"
+      v-ripple="false"
+      text
+      @click.stop="$emit('open')">
+      <span class="text-body white--text text-center">{{ $t('agenda.timeline.seeMore') }}</span>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    attendees: {
+      type: Array,
+      default: () => [],
+    },
+    max: {
+      type: Number,
+      default: 3,
+    },
+    size: {
+      type: Number,
+      default: 34,
+    },
+  },
+  data() {
+    return {
+      showSeeMore: false,
+    };
+  },
+  computed: {
+    visibleAttendees() {
+      return this.attendees.slice(0, this.max);
+    },
+    overflowCount() {
+      return Math.max(0, this.attendees.length - this.max);
+    },
+    avatarsWidth() {
+      const count = Math.min(this.attendees.length, this.max) + (this.overflowCount > 0 ? 1 : 0);
+      return this.size + (count - 1) * (this.size - 12) + (count * 2);
+    },
+    defaultAvatarUrl() {
+      return '/portal/rest/v1/social/users/default-image/avatar';
+    },
+  },
+  methods: {
+    attendeeAvatarUrl(attendee) {
+      const profile = attendee.identity.profile || attendee.identity.space || {};
+      return profile.avatar || profile.avatarUrl || '/portal/rest/v1/social/users/default-image/avatar';
+    },
+  },
+};
+</script>
---
+++ agenda-webapps/src/main/webapp/vue-app/agenda-common/components/event/form/AgendaEventFormAttendeesDrawer.vue
@@ -0,0 +1,227 @@
+<template>
+  <exo-drawer
+    ref="attendeesDrawer"
+    right
+    body-classes="hide-scroll decrease-z-index-more"
+    @closed="$emit('closed')">
+    <template slot="title">
+      {{ $t('agenda.label.participants') }}
+    </template>
+    <template slot="content">
+      <div class="pa-4">
+        <template v-if="editable">
+          <div v-if="!showSuggester">
+            <v-btn
+              color="primary"
+              small
+              class="mb-4"
+              @click="showSuggester = true">
+              <v-icon size="12" class="me-1">fas fa-plus</v-icon>
+              {{ $t('agenda.label.addParticipants') }}
+            </v-btn>
+          </div>
+          <form
+            v-else
+            ref="form"
+            class="mb-4"
+            @keypress="checkGuestInvitation($event)">
+            <div class="d-flex align-center">
+              <v-btn icon x-small class="me-2 flex-shrink-0" @click="showSuggester = false">
+                <v-icon size="16" class="icon-default-color">fas fa-arrow-left</v-icon>
+              </v-btn>
+              <exo-identity-suggester
+                ref="invitedAttendeeAutoComplete"
+                v-model="invitedAttendee"
+                :labels="participantSuggesterLabels"
+                :title="suggesterStatus"
+                :disabled="disableAttendeeSuggester"
+                :ignore-items="ignoredMembers"
+                :search-options="searchOptions"
+                name="inviteAttendee"
+                no-redactor-space
+                include-users
+                dense
+                include-spaces />
+            </div>
+            <span v-if="disableAttendeeSuggester" class="error--text caption">
+              {{ $t('agenda.suggesterRequired') }}
+            </span>
+          </form>
+        </template>
+        <div v-if="event && event.attendees" class="mt-2">
+          <agenda-event-form-attendee-item
+            v-for="attendee in displayedAttendees"
+            :key="attendee.identity.id || attendee.identity.remoteId"
+            :attendee="attendee"
+            :creator="event.creator"
+            :editable="editable"
+            @remove-attendee="removeAttendee" />
+          <div v-if="hasMore" class="d-flex justify-center py-2">
+            <v-btn text small @click="loadMore">
+              {{ $t('agenda.button.loadMore') }}
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </template>
+  </exo-drawer>
+</template>
+
+<script>
+const PAGE_SIZE = 20;
+
+export default {
+  props: {
+    event: {
+      type: Object,
+      default: () => ({}),
+    },
+    editable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      invitedAttendee: null,
+      displayedCount: PAGE_SIZE,
+      showSuggester: false,
+    };
+  },
+  computed: {
+    sortedAttendees() {
+      const attendees = this.event && this.event.attendees || [];
+      if (!attendees.length) {
+        return [];
+      }
+      const creatorId = (this.event && this.event.creator && this.event.creator.id)
+        || eXo.env.portal.userIdentityId;
+      const owner = attendees.find(a => Number(a.identity.id) === Number(creatorId));
+      const others = attendees
+        .filter(a => Number(a.identity.id) !== Number(creatorId))
+        .sort((a1, a2) => {
+          const n1 = (a1.identity.profile && (a1.identity.profile.fullname || a1.identity.profile.fullName))
+            || (a1.identity.space && a1.identity.space.displayName) || '';
+          const n2 = (a2.identity.profile && (a2.identity.profile.fullname || a2.identity.profile.fullName))
+            || (a2.identity.space && a2.identity.space.displayName) || '';
+          return n1.localeCompare(n2);
+        });
+      return owner ? [owner, ...others] : others;
+    },
+    displayedAttendees() {
+      return this.sortedAttendees.slice(0, this.displayedCount);
+    },
+    hasMore() {
+      return this.sortedAttendees.length > this.displayedCount;
+    },
+    searchOptions() {
+      return {
+        currentUser: '',
+        spaceURL: this.event
+          && this.event.calendar
+          && this.event.calendar.owner
+          && this.event.calendar.owner.remoteId,
+      };
+    },
+    participantSuggesterLabels() {
+      return {
+        searchPlaceholder: this.$t('agenda.searchPlaceholder'),
+        placeholder: this.$t('agenda.attendees.searchPlaceholder'),
+        noDataLabel: this.$t('agenda.noDataLabel'),
+      };
+    },
+    ignoredMembers() {
+      return this.event.attendees
+        ? this.event.attendees.map(a => `${a.identity.providerId}:${a.identity.remoteId}`)
+        : [];
+    },
+    disableAttendeeSuggester() {
+      return !this.event.calendar || !this.event.calendar.owner || !this.event.calendar.owner.remoteId;
+    },
+    suggesterStatus() {
+      return this.disableAttendeeSuggester ? this.$t('agenda.suggesterRequired.tooltip') : '';
+    },
+  },
+  watch: {
+    invitedAttendee(val) {
+      if (!val) {
+        this.$nextTick(this.$refs.invitedAttendeeAutoComplete.$refs.selectAutoComplete.deleteCurrentItem);
+        return;
+      }
+      if (!this.event.attendees) {
+        this.event.attendees = [];
+      }
+      const found = this.event.attendees.find(a =>
+        a.identity.remoteId === val.remoteId && a.identity.providerId === val.providerId
+      );
+      if (!found) {
+        this.event.attendees.push({
+          identity: this.$suggesterService.convertSuggesterItemToIdentity(val),
+          response: 'NEEDS_ACTION',
+        });
+      }
+      this.invitedAttendee = null;
+    },
+  },
+  methods: {
+    open() {
+      this.displayedCount = PAGE_SIZE;
+      this.showSuggester = false;
+      this.$refs.attendeesDrawer.open();
+    },
+    loadMore() {
+      this.displayedCount += PAGE_SIZE;
+    },
+    removeAttendee(attendee) {
+      if (!this.event || !this.event.attendees) {
+        return;
+      }
+      const index = this.event.attendees.findIndex(a =>
+        a.identity.remoteId === attendee.identity.remoteId
+        && a.identity.providerId === attendee.identity.providerId
+      );
+      if (index >= 0) {
+        this.event.attendees.splice(index, 1);
+      }
+    },
+    checkGuestInvitation(evt) {
+      const self = this;
+      $('form').on('focusout', function(event) {
+        setTimeout(function() {
+          if (!event.delegateTarget.contains(document.activeElement)) {
+            self.saveGuestEmail(event);
+          }
+        }, 1);
+      });
+      if (evt.key === 'Enter') {
+        evt.preventDefault();
+        this.saveGuestEmail(evt);
+      }
+      if (evt.keyCode === 32) {
+        this.saveGuestEmail(evt);
+      }
+    },
+    saveGuestEmail() {
+      const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+      const input = this.$refs?.invitedAttendeeAutoComplete?.searchTerm?.toLowerCase();
+      const words = input !== null ? input.split(' ') : '';
+      const email = words[words.length - 1];
+      if (reg.test(email)) {
+        this.event.attendees.push({
+          identity: {
+            id: `${email}`,
+            remoteId: email,
+            identityId: email,
+            providerId: 'GUEST_USER',
+            profile: {
+              fullName: email,
+              avatarUrl: '/portal/rest/v1/social/users/default-image/avatar',
+            },
+          },
+        });
+      }
+      this.$refs.invitedAttendeeAutoComplete.clear();
+    },
+  },
+};
+</script>
