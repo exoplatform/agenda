6a9edbbe7f05157dd20331bf758008512cbc6511
Ayoub Zayati <azayati@exoplatform.com>
2026-09-01 11:16:36 +0100

    feat: Declare the Agenda digest mail notifications category - EXO-89484 - eXIP7.3.0.22 (#1070)
    (cherry picked from commit 8aedd2773f102ffe2127eafc686a303935bc4bcd)
    (cherry picked from commit 24854c653d939c1782237c96b858835dbe8cda77)
    (cherry picked from commit 9a7fcc3cf93ad2cfa7dd0bf4fd637f7ba175f187)

--- agenda-api/src/main/java/org/exoplatform/agenda/service/AgendaUserSettingsService.java
+++ agenda-api/src/main/java/org/exoplatform/agenda/service/AgendaUserSettingsService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.exoplatform.agenda.constant.AvailabilitySharing;
 import org.exoplatform.agenda.model.AgendaUserSettings;
 import org.exoplatform.agenda.model.EventReminderParameter;
-import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.social.core.identity.model.Identity;
 
 public interface AgendaUserSettingsService {
@@ -59,15 +58,6 @@ public interface AgendaUserSettingsService {
    */
   List<EventReminderParameter> getDefaultReminders();
 
-
-  /**
-   * Update the user TimeZONE
-   *
-   * @param userName  userName
-   * @param timeZone  timeZone
-   * @throws ObjectNotFoundException when user profile is not found
-   */
-  void updateUserTimeZone(String userName, String timeZone) throws ObjectNotFoundException;
 
   /**
    * Retrieves the globally configured embed map provider identifier.
